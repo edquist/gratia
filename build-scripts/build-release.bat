d916faa9e031114280f4230f8804c54821ba8a8c
glr01 <glr01@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2006-06-15 10:17:01 +0000

    glr: updated to create vdt install tarballs
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@38 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- build-scripts/build-release.bat
+++ build-scripts/build-release.bat
@@ -6,7 +6,4 @@ call build-servlets.bat
 call build-reporting.bat
 call build-reports.bat
 call build-configurator.bat
-copy %root%\configuration\* %root%\target
-copy %root%\configuration-release\* %root%\target
-copy %root%\wars\* %root%\target
-
+call build-tarballs.bat
