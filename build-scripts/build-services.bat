268d9734ea05d88ed9d49ae6860a34d09b5d61b6
glr01 <glr01@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2006-07-06 13:31:24 +0000

    glr: fixed storage build
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@49 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- build-scripts/build-services.bat
+++ build-scripts/build-services.bat
@@ -16,15 +16,15 @@ mkdir war\WEB-INF\lib
 rmdir /q /s net
 
 javac -extdirs %root%\jars %root%\services\net\sf\gratia\services\*.java
-javac -extdirs %root%\jars %root%\storage\net\sf\gratia\storage\*.java
+javac -extdirs %root%\jars %root%\services\net\sf\gratia\storage\*.java
 
 rmic -d . net.sf.gratia.services.JMSProxyImpl
 copy net\sf\gratia\services\* %root%\services\net\sf\gratia\services
 
 rmdir /q /s net
 
-copy %root%\services%\net\sf\gratia\services\*.class war\WEB-INF\classes\net\sf\gratia\services
-copy %root%\storage\net\sf\gratia\storage\*.class war\WEB-INF\classes\net\sf\gratia\storage
+copy %root%\services\net\sf\gratia\services\*.class war\WEB-INF\classes\net\sf\gratia\services
+copy %root%\services\net\sf\gratia\storage\*.class war\WEB-INF\classes\net\sf\gratia\storage
 
 copy %root%\jars\* war\WEB-INF\lib
 del war\WEB-INF\lib\serv*
