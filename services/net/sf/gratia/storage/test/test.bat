d9cfc2a99d14ffe2557b6d328a9564bdc6d73ca3
pcanal <pcanal@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2006-08-17 17:42:16 +0000

    add needed library
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@123 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- services/net/sf/gratia/storage/test/test.bat
+++ services/net/sf/gratia/storage/test/test.bat
@@ -1,9 +1,9 @@
 set root=..\..\..\..\..\..\
 dir %root%
-set classpath=%root%services;jars\antlr-2.7.5.jar;jars\bsh.jar;jars\derby.jar;jars\derbytools.jar;jars\hibernate3.jar
-set classpath=%classpath%;jars\dom4j-1.6.1.jar;jars\commons-logging-1.0.4.jar
-set classpath=%classpath%;jars\commons-collections-2.1.1.jar;jars\cglib-2.1.3.jar
-set classpath=%classpath%;jars\ehcache-1.1.jar;jars\jta.jar
+set classpath=%root%services;%root%jars\antlr-2.7.5.jar;jars\bsh.jar;jars\derby.jar;jars\derbytools.jar;%root%jars\hibernate3.jar
+set classpath=%classpath%;%root%jars\dom4j-1.6.1.jar;%root%jars\commons-logging-1.0.4.jar
+set classpath=%classpath%;%root%jars\commons-collections-2.1.1.jar;%root%jars\cglib-2.1.3.jar
+set classpath=%classpath%;%root%jars\ehcache-1.1.jar;%root%jars\jta.jar;%root%jars\jaxen-1.1-beta-7.jar
 
 del *~
 del *#
@@ -15,6 +15,7 @@ mkdir output
 javac %root%services\net\sf\gratia\services\XP.java
 javac %root%services\net\sf\gratia\services\Logging.java
 javac %root%services\net\sf\gratia\storage\*.java
+
 copy %root%configuration\JobUsage.hbm.xml
 
 java bsh.Interpreter test.bsh
