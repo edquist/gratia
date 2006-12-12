84d17a14bb8e79f6c3ec24c84fc06acadb56f745
glr01 <glr01@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2006-12-12 11:55:24 +0000

    glr: added
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@341 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- build-scripts/build-gratia.bat
+++
@@ -1,6 +0,0 @@
-call setup.bat
-mkdir %catalina_home%\gratia
-copy %root%\configuration\* %catalina_home%\gratia
-del %catalina_home%\gratia\local.*
-del %catalina_home%\gratia\release.*
-copy %root%\configuration\local.service-configuration.properties %catalina_home%\gratia\service-configuration.properties
