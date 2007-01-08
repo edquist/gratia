77428333463038569e7103ca354cefe5b6233964
glr01 <glr01@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-01-08 19:51:05 +0000

    glr: create stored procedures on initial startup
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@420 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- configuration/post-install.sh
+++ configuration/post-install.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
-cd CATALINA_HOME/gratia
-mysql -v --f1gorce -unbuffered --user=root --host=localhost --port=3320 gratia < build-summary-tables.sql
-mysql -v --force -unbuffered --user=root --host=localhost --port=3320 gratia < build-stored-procedures.sql
-mysql -v --force -unbuffered --user=root --host=localhost --port=3320 gratia < build-roles-table.sql
+#
+# Create the mysql gratia summary tables and stored procedures
+# Needs to be executed after Tomcat has started so that the war files are unpacked 
+#	and the hibernate has created the gratia schema tables.
+#
+
+mysql -v --force -unbuffered --user=root --password=ROOTPASS  --host=localhost --port=PORT gratia < MAGIC_VDT_LOCATION/tomcat/v55/gratia/build-summary-tables.sql
+mysql -v --force -unbuffered --user=root --password=ROOTPASS  --host=localhost --port=PORT gratia < MAGIC_VDT_LOCATION/tomcat/v55/gratia/build-stored-procedures.sql
+mysql -v --force -unbuffered --user=root --password=ROOTPASS  --host=localhost --port=PORT gratia < MAGIC_VDT_LOCATION/tomcat/v55/gratia/build-roles-table.sql
+
