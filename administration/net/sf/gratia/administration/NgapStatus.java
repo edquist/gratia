e08a13211e81ea92872e352800f06f6b557a4088
glr01 <glr01@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-01-08 19:45:48 +0000

    glr: updates to add status reporting for ngap/roles table updating
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@417 c85a5f45-a5f3-44fc-b02c-7891c381c519

---
+++ administration/net/sf/gratia/administration/NgapStatus.java
@@ -0,0 +1,202 @@
+package net.sf.gratia.administration;
+
+import net.sf.gratia.services.*;
+
+import java.io.*;
+import java.net.*;
+
+import java.util.StringTokenizer;
+import java.util.Properties;
+import java.util.Hashtable;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.sql.*;
+
+import java.util.regex.*;
+import java.text.*;
+
+public class NgapStatus extends HttpServlet 
+{
+		XP xp = new XP();
+		
+
+		//
+		// database related
+		//
+		String driver = "";
+		String url = "";
+		String user = "";
+		String password = "";
+		Connection connection;
+		Statement statement;
+		ResultSet resultSet;
+		//
+		// processing related
+		//
+		StringBuffer buffer = new StringBuffer();
+		//
+		// globals
+		//
+		HttpServletRequest request;
+		HttpServletResponse response;
+		boolean initialized = false;
+		Properties props;
+		String message = null;
+		//
+		// support
+		//
+		String dq = "\"";
+		String comma = ",";
+		String cr = "\n\r";
+
+    public void init(ServletConfig config) throws ServletException 
+		{
+    }
+    
+		public void openConnection()
+		{
+				try
+						{
+								props = Configuration.getProperties();
+								driver = props.getProperty("service.mysql.driver");
+								url = props.getProperty("service.mysql.url");
+								user = props.getProperty("service.mysql.user");
+								password = props.getProperty("service.mysql.password");
+						}
+				catch (Exception ignore)
+						{
+						}
+				try
+						{
+								Class.forName(driver).newInstance();
+								connection = DriverManager.getConnection(url,user,password);
+						}
+				catch (Exception e)
+						{
+								e.printStackTrace();
+						}
+		}
+
+		public void closeConnection()
+		{
+				try
+						{
+								connection.close();
+						}
+				catch (Exception e)
+						{
+								e.printStackTrace();
+						}
+		}
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+		{
+				openConnection();
+
+				this.request = request;
+				this.response = response;
+				buffer = new StringBuffer();
+				process();
+				response.setContentType("text/html");
+				response.setHeader("Cache-Control", "no-cache"); // HTTP 1.1
+				response.setHeader("Pragma", "no-cache"); // HTTP 1.0
+				PrintWriter writer = response.getWriter();
+				writer.write(buffer.toString());
+				writer.flush();
+				writer.close();
+				closeConnection();
+		}
+
+		public void process()
+		{
+				int index = 0;
+				String command = "";
+				String dq = "'";
+
+				Integer count1 = null;
+				Integer error1 = null;
+
+				Integer count24 = null;
+				Integer error24 = null;
+
+				java.util.Date now = new java.util.Date();
+				long decrement = 60 * 60 * 1000;
+				java.util.Date to = new java.util.Date();
+				java.util.Date from = new java.util.Date(to.getTime() - decrement);
+
+				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss");
+
+				try
+						{
+								//
+								// previous hour
+								//
+
+								command = "select count(*) from JobUsageRecord where ServerDate > " + dq + format.format(from) + dq +
+										" and ServerDate <= " + dq + format.format(to) + dq;
+								System.out.println("command: " + command);
+								statement = connection.prepareStatement(command);
+								resultSet = statement.executeQuery(command);
+								while(resultSet.next())
+										count1 = resultSet.getInt(1);
+								resultSet.close();
+								statement.close();
+
+								command = "select count(*) from DupRecord where EventDate > " + dq + format.format(from) + dq +
+										" and EventDate <= " + dq + format.format(to) + dq;
+								statement = connection.prepareStatement(command);
+								resultSet = statement.executeQuery(command);
+								while(resultSet.next())
+										error1 = resultSet.getInt(1);
+								resultSet.close();
+								statement.close();
+
+								//
+								// previous day
+								//
+
+								decrement = 24 * 60 * 60 * 1000;
+								java.util.Date date = new java.util.Date(now.getTime() - decrement);
+
+								command = "select count(*) from JobUsageRecord where ServerDate > " + dq + format.format(date) + dq;
+								statement = connection.prepareStatement(command);
+								resultSet = statement.executeQuery(command);
+								while(resultSet.next())
+										count24 = resultSet.getInt(1);
+								resultSet.close();
+								statement.close();
+
+								command = "select count(*) from DupRecord where EventDate > " + dq + format.format(date) + dq;
+								statement = connection.prepareStatement(command);
+								resultSet = statement.executeQuery(command);
+								while(resultSet.next())
+										error24 = resultSet.getInt(1);
+								resultSet.close();
+								statement.close();
+
+						}
+				catch (Exception e)
+						{
+								e.printStackTrace();
+						}
+
+				buffer.append("record-count-hour|" + count1 + cr);
+				buffer.append("record-count-24hour|" + count24 + cr);
+				
+				int maxthreads = Integer.parseInt(props.getProperty("service.listener.threads"));
+				String path = System.getProperties().getProperty("catalina.home");
+				path = xp.replaceAll(path,"\\","/");
+
+				for (int i = 0; i < maxthreads; i++)
+						{
+								String xpath = path + "/gratia/data/thread" + i;
+								String filelist[] = xp.getFileList(xpath);
+								buffer.append("queuesize" + i + "|" + filelist.length + cr);
+						}
+		}
+
+}
---
+++ administration/net/sf/gratia/administration/Roles.java
@@ -0,0 +1,355 @@
+package net.sf.gratia.administration;
+
+import net.sf.gratia.services.*;
+
+import java.io.*;
+import java.net.*;
+
+import java.util.StringTokenizer;
+import java.util.Properties;
+import java.util.Hashtable;
+import java.util.Enumeration;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.sql.*;
+
+import java.util.regex.*;
+
+public class Roles extends HttpServlet 
+{
+		XP xp = new XP();
+		//
+		// database related
+		//
+		String driver = "";
+		String url = "";
+		String user = "";
+		String password = "";
+		Connection connection;
+		Statement statement;
+		ResultSet resultSet;
+		//
+		// processing related
+		//
+		String html = "";
+		String row = "";
+		Pattern p = Pattern.compile("<tr>.*?</tr>",Pattern.MULTILINE + Pattern.DOTALL);
+		Matcher m = null;
+		StringBuffer buffer = new StringBuffer();
+		//
+		// globals
+		//
+		HttpServletRequest request;
+		HttpServletResponse response;
+		boolean initialized = false;
+		//
+		// support
+		//
+		String dq = "\"";
+		String comma = ",";
+		String cr = "\n";
+		Hashtable table = new Hashtable();
+		Hashtable cetablebyid = new Hashtable();
+		Hashtable cetablebyname = new Hashtable();
+		String newname = "<New Probe Name>";
+
+    public void init(ServletConfig config) throws ServletException 
+		{
+    }
+    
+		public void openConnection()
+		{
+				try
+						{
+								Properties p = Configuration.getProperties();
+								driver = p.getProperty("service.mysql.driver");
+								url = p.getProperty("service.mysql.url");
+								user = p.getProperty("service.mysql.user");
+								password = p.getProperty("service.mysql.password");
+						}
+				catch (Exception ignore)
+						{
+						}
+				try
+						{
+								Class.forName(driver).newInstance();
+								connection = DriverManager.getConnection(url,user,password);
+						}
+				catch (Exception e)
+						{
+								e.printStackTrace();
+						}
+		}
+
+		public void closeConnection()
+		{
+				try
+						{
+								connection.close();
+						}
+				catch (Exception e)
+						{
+								e.printStackTrace();
+						}
+		}
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+		{
+				openConnection();
+
+				this.request = request;
+				this.response = response;
+				table = new Hashtable();
+				if (request.getParameter("action") != null)
+						{
+								if (request.getParameter("action").equals("delete"))
+										delete();
+						}
+				setup();
+				process();
+				response.setContentType("text/html");
+				response.setHeader("Cache-Control", "no-cache"); // HTTP 1.1
+				response.setHeader("Pragma", "no-cache"); // HTTP 1.0
+				request.getSession().setAttribute("table",table);
+				PrintWriter writer = response.getWriter();
+				writer.write(html);
+				writer.flush();
+				writer.close();
+				closeConnection();
+		}
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+		{
+				openConnection();
+				this.request = request;
+				this.response = response;
+				table = (Hashtable) request.getSession().getAttribute("table");
+				update();
+				closeConnection();
+				response.sendRedirect("roles.html");
+		}
+
+		public void setup()
+		{
+				html = xp.get(request.getRealPath("/") + "roles.html");
+				m = p.matcher(html);
+				while (m.find())
+						{
+								String temp = m.group();
+								if (temp.indexOf("#index#") > 0)
+										{
+												row = temp;
+												break;
+										}
+						}
+		}
+
+		public void process()
+		{
+				int index = 0;
+				String command = "";
+				buffer = new StringBuffer();
+
+				try
+						{
+								command = "select * from RolesTable";
+								statement = connection.prepareStatement(command);
+								resultSet = statement.executeQuery(command);
+
+								while(resultSet.next())
+										{
+												String newrow = new String(row);
+
+												newrow = xp.replaceAll(newrow,"#index#","" + index);
+												table.put("index:" + index,"" + index);
+
+												newrow = xp.replaceAll(newrow,"#roleid#",resultSet.getString("roleid"));
+												table.put("roleid:" + index,resultSet.getString("roleid"));
+
+												String role = resultSet.getString("role");
+												newrow = xp.replaceAll(newrow,"#role#",role);
+												table.put("role:" + index,role);
+
+												String subtitle = resultSet.getString("subtitle");
+												newrow = xp.replaceAll(newrow,"#subtitle#",subtitle);
+												table.put("subtitle:" + index,subtitle);
+
+												String where = resultSet.getString("whereclause");
+												newrow = xp.replaceAll(newrow,"#where#",where);
+												table.put("where:" + index,where);
+
+												buffer.append(newrow);
+												index++;
+										}
+								resultSet.close();
+								statement.close();
+						}
+				catch (Exception e)
+						{
+								e.printStackTrace();
+						}
+
+				String newrow = new String(row);
+				newrow = xp.replaceAll(newrow,"#index#","" + index);
+				newrow = xp.replace(newrow,"#role#","New Role");
+				newrow = xp.replace(newrow,"#subtitle#","");
+				newrow = xp.replace(newrow,"#where#","");
+				table.put("index:" + index,"" + index);
+				table.put("role:" + index,"New Role");
+				buffer.append(newrow);
+				html = xp.replace(html,row,buffer.toString());
+		}
+
+
+		public void update()
+		{
+				int index;
+				String key = "";
+				String oldvalue = "";
+				String newvalue = "";
+
+				/*
+				Enumeration x = request.getParameterNames();
+				while(x.hasMoreElements())
+						{
+								key = (String) x.nextElement();
+								String value = (String) request.getParameter(key);
+								System.out.println("key: " + key + " value: " + value);
+						}
+				*/
+
+				for (index = 0; index < 1000; index++)
+						{
+
+								key = "index:" + index;
+								oldvalue = (String) table.get(key);
+								newvalue = (String) request.getParameter(key);
+								if (oldvalue == null)
+										{
+												break;
+										}
+								
+								key = "role:" + index;
+								oldvalue = (String) table.get(key);
+								newvalue = (String) request.getParameter(key);
+								
+								if ((oldvalue != null) && (oldvalue.equals("New Role")) && (! oldvalue.equals(newvalue)))
+										{
+												insert(index);
+												continue;
+										}
+
+								key = "subtitle:" + index;
+								oldvalue = (String) table.get(key);
+								newvalue = (String) request.getParameter(key);
+								if (! oldvalue.equals(newvalue))
+										{
+												update(index);
+												continue;
+										}
+
+								key = "where:" + index;
+								oldvalue = (String) table.get(key);
+								newvalue = (String) request.getParameter(key);
+								if (! oldvalue.equals(newvalue))
+										{
+												update(index);
+												continue;
+										}
+
+						}
+		}
+
+		public void update(int index)
+		{
+				String roleid = (String) request.getParameter("roleid:" + index);
+				String role = (String) request.getParameter("role:" + index);
+				String subtitle = (String) request.getParameter("subtitle:" + index);
+				String where = (String) request.getParameter("where:" + index);
+
+				String command = 
+						"update RolesTable set" + cr +
+						" role = " + dq + role + dq + comma + cr +
+						" subtitle = " + dq + subtitle + dq + comma + cr +
+						" whereclause = " + dq + where + dq + cr +
+						" where roleid = " + roleid;
+				try
+						{
+								statement = connection.createStatement();
+								statement.executeUpdate(command);
+								statement.close();
+						}
+				catch (Exception e)
+						{
+								e.printStackTrace();
+						}
+				finally
+						{
+								try
+										{
+												statement.close();
+										}
+								catch (Exception ignore)
+										{
+										}
+						}
+		}
+
+		public void insert(int index)
+		{
+				String role = (String) request.getParameter("role:" + index);
+				String subtitle = (String) request.getParameter("subtitle:" + index);
+				String where = (String) request.getParameter("where:" + index);
+
+				String command = 
+						"insert into RolesTable" +
+						"(role,subtitle,whereclause)" + cr +
+						"values(" + cr +
+						dq + role + dq + comma + cr +
+						dq + subtitle + dq + comma + cr +
+						dq + where + dq + ")";
+
+				try
+						{
+								statement = connection.createStatement();
+								statement.executeUpdate(command);
+								statement.close();
+						}
+				catch (Exception e)
+						{
+								System.out.println("command: " + command);
+								e.printStackTrace();
+						}
+				finally
+						{
+								try
+										{
+												statement.close();
+										}
+								catch (Exception ignore)
+										{
+										}
+						}
+		}
+
+		void delete()
+		{
+				String command = "";
+
+				try
+						{
+								command = "delete from RolesTable where roleid = " + request.getParameter("roleid");
+								Statement statement = connection.createStatement();
+								statement.executeUpdate(command);
+								statement.close();
+						}
+				catch (Exception e)
+						{
+								System.out.println("command: " + command);
+								e.printStackTrace();
+						}
+		}
+
+}
