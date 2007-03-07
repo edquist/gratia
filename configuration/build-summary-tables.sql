b17b6c58bb21c7568c47943ed71006d1965124a8
greenc <greenc@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-03-07 19:50:44 +0000

    Add ResourceType attribute.
    Remove ^M.
    
    
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@494 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- configuration/build-summary-tables.sql
+++ configuration/build-summary-tables.sql
@@ -1,156 +1,177 @@
 delimiter ||
 
-drop table if exists ProbeSummary, UserProbeSummary, VOProbeSummary, ProbeStatus, HostDescriptionProbeSummary;
+drop table if exists ProbeSummary, UserProbeSummary, VOProbeSummary,
+	ProbeStatus, HostDescriptionProbeSummary;
 ||
 CREATE TABLE `ProbeStatus` (
-  `EndTime` DATETIME NOT NULL DEFAULT 0,
-  `ProbeName` VARCHAR(255) NOT NULL DEFAULT '',
-  `Njobs` INTEGER NOT NULL DEFAULT 0
+	`EndTime` DATETIME NOT NULL DEFAULT 0,
+	`ProbeName` VARCHAR(255) NOT NULL DEFAULT '',
+	`Njobs` INTEGER NOT NULL DEFAULT 0
 );
 ||
 CREATE TABLE `ProbeSummary` (
-  `EndTime` DATETIME NOT NULL DEFAULT 0,
-  `ProbeName` VARCHAR(255) NOT NULL DEFAULT '',
-  `SiteName` VARCHAR(255) DEFAULT 'Unknown',
-  `Njobs` INTEGER NOT NULL DEFAULT 0,
-  `WallDuration` DOUBLE NOT NULL DEFAULT 0,
-  `CpuUserDuration` DOUBLE NOT NULL DEFAULT 0,
-  `CpuSystemDuration` DOUBLE NOT NULL DEFAULT 0
+	`EndTime` DATETIME NOT NULL DEFAULT 0,
+	`ProbeName` VARCHAR(255) NOT NULL DEFAULT '',
+	`SiteName` VARCHAR(255) DEFAULT 'Unknown',
+	`ResourceType` VARCHAR(255) DEFAULT 'Unknown',
+	`Njobs` INTEGER NOT NULL DEFAULT 0,
+	`WallDuration` DOUBLE NOT NULL DEFAULT 0,
+	`CpuUserDuration` DOUBLE NOT NULL DEFAULT 0,
+	`CpuSystemDuration` DOUBLE NOT NULL DEFAULT 0
 );
 ||
 CREATE TABLE `UserProbeSummary` (
-  `EndTime` DATETIME NOT NULL DEFAULT 0,
-  `CommonName` VARCHAR(255) DEFAULT 'Unknown',
-  `ProbeName` VARCHAR(255) NOT NULL DEFAULT '',
-  `Njobs` INTEGER NOT NULL DEFAULT 0,
-  `WallDuration` DOUBLE NOT NULL DEFAULT 0,
-  `CpuUserDuration` DOUBLE NOT NULL DEFAULT 0,
-  `CpuSystemDuration` DOUBLE NOT NULL DEFAULT 0
+	`EndTime` DATETIME NOT NULL DEFAULT 0,
+	`CommonName` VARCHAR(255) DEFAULT 'Unknown',
+	`ProbeName` VARCHAR(255) NOT NULL DEFAULT '',
+	`ResourceType` VARCHAR(255) DEFAULT 'Unknown',
+	`Njobs` INTEGER NOT NULL DEFAULT 0,
+	`WallDuration` DOUBLE NOT NULL DEFAULT 0,
+	`CpuUserDuration` DOUBLE NOT NULL DEFAULT 0,
+	`CpuSystemDuration` DOUBLE NOT NULL DEFAULT 0
 );
 ||
 CREATE TABLE `VOProbeSummary` (
-  `EndTime` DATETIME NOT NULL DEFAULT 0,
-  `VOName` VARCHAR(255) DEFAULT 'Unknown',
-  `ProbeName` VARCHAR(255) NOT NULL DEFAULT '',
-  `Njobs` INTEGER NOT NULL DEFAULT 0,
-  `WallDuration` DOUBLE NOT NULL DEFAULT 0,
-  `CpuUserDuration` DOUBLE NOT NULL DEFAULT 0,
-  `CpuSystemDuration` DOUBLE NOT NULL DEFAULT 0
+	`EndTime` DATETIME NOT NULL DEFAULT 0,
+	`VOName` VARCHAR(255) DEFAULT 'Unknown',
+	`ProbeName` VARCHAR(255) NOT NULL DEFAULT '',
+	`ResourceType` VARCHAR(255) DEFAULT 'Unknown',
+	`Njobs` INTEGER NOT NULL DEFAULT 0,
+	`WallDuration` DOUBLE NOT NULL DEFAULT 0,
+	`CpuUserDuration` DOUBLE NOT NULL DEFAULT 0,
+	`CpuSystemDuration` DOUBLE NOT NULL DEFAULT 0
 );
 ||
 CREATE TABLE `HostDescriptionProbeSummary` (
-  `EndTime` DATETIME NOT NULL DEFAULT 0,
-  `HostDescription` VARCHAR(255) DEFAULT 'Unknown',
-  `ProbeName` VARCHAR(255) NOT NULL DEFAULT '',
-  `Njobs` INTEGER NOT NULL DEFAULT 0,
-  `WallDuration` DOUBLE NOT NULL DEFAULT 0,
-  `CpuUserDuration` DOUBLE NOT NULL DEFAULT 0,
-  `CpuSystemDuration` DOUBLE NOT NULL DEFAULT 0
+	`EndTime` DATETIME NOT NULL DEFAULT 0,
+	`HostDescription` VARCHAR(255) DEFAULT 'Unknown',
+	`ProbeName` VARCHAR(255) NOT NULL DEFAULT '',
+	`ResourceType` VARCHAR(255) DEFAULT 'Unknown',
+	`Njobs` INTEGER NOT NULL DEFAULT 0,
+	`WallDuration` DOUBLE NOT NULL DEFAULT 0,
+	`CpuUserDuration` DOUBLE NOT NULL DEFAULT 0,
+	`CpuSystemDuration` DOUBLE NOT NULL DEFAULT 0
 );
 ||
 insert into ProbeStatus
-  (select
-    str_to_date(date_format(ServerDate,'%Y-%c-%e %H:00:00'),'%Y-%c-%e %H:00:00') as EndTime,
-    ProbeName,
-    count(*) as Njobs
-    from JobUsageRecord
-    group by ProbeName,str_to_date(date_format(ServerDate,'%Y-%c-%e %H:00:00'),'%Y-%c-%e %H:00:00')
+	(select
+		str_to_date(date_format(ServerDate,'%Y-%c-%e %H:00:00'),'%Y-%c-%e %H:00:00') as EndTime,
+		ProbeName,
+		count(*) as Njobs
+		from JobUsageRecord
+		group by ProbeName,str_to_date(date_format(ServerDate,'%Y-%c-%e %H:00:00'),'%Y-%c-%e %H:00:00')
 );
 ||
 alter table ProbeStatus
-  add index index01(EndTime);
+	add index index01(EndTime);
 ||
 alter table ProbeStatus
-  add index index02(ProbeName);
+	add index index02(ProbeName);
 ||
 insert into ProbeSummary
-  (select
-    date(EndTime) as EndTime,
-    ProbeName,
+	(select
+		date(EndTime) as EndTime,
+		ProbeName,
 		SiteName,
-    sum(Njobs) as Njobs,
-    sum(WallDuration) as WallDuration,
-    sum(CpuUserDuration) as CpuUserDuration,
-    sum(CpuSystemDuration) as CpuSystemDuration
-    from JobUsageRecord
+		ResourceType,
+		sum(Njobs) as Njobs,
+		sum(WallDuration) as WallDuration,
+		sum(CpuUserDuration) as CpuUserDuration,
+		sum(CpuSystemDuration) as CpuSystemDuration
+		from JobUsageRecord
 		where CpuUserDuration is not null
-    group by ProbeName,date(EndTime)
+		group by ProbeName,ResourceType,date(EndTime)
 );
-
-alter table ProbeSummary
-  add index index01(EndTime);
 ||
 alter table ProbeSummary
-  add index index02(ProbeName);
+	add index index01(EndTime);
+||
+alter table ProbeSummary
+	add index index02(ProbeName);
+||
+alter table ProbeSummary
+	add index index03(ResourceType);
 ||
 insert into UserProbeSummary
-  (select
-    date(EndTime) as EndTime,
-    CommonName,
-    JobUsageRecord.ProbeName,
-    sum(Njobs) as Njobs,
-    sum(WallDuration) as WallDuration,
-    sum(CpuUserDuration) as CpuUserDuration,
-    sum(CpuSystemDuration) as CpuSystemDuration
-    from JobUsageRecord
+	(select
+		date(EndTime) as EndTime,
+		CommonName,
+		ProbeName,
+		ResourceType,
+		sum(Njobs) as Njobs,
+		sum(WallDuration) as WallDuration,
+		sum(CpuUserDuration) as CpuUserDuration,
+		sum(CpuSystemDuration) as CpuSystemDuration
+		from JobUsageRecord
 		where CpuUserDuration is not null
-    group by CommonName,ProbeName,date(EndTime)
+		group by CommonName,ProbeName,ResourceType,date(EndTime)
 );
 ||
 alter table UserProbeSummary
-  add index index01(EndTime);
+	add index index01(EndTime);
 ||
 alter table UserProbeSummary
-  add index index02(CommonName);
+	add index index02(CommonName);
 ||
 alter table UserProbeSummary
-  add index index03(ProbeName);
+	add index index03(ProbeName);
+||
+alter table UserProbeSummary
+	add index index04(ResourceType);
 ||
 insert into VOProbeSummary
-  (select
-    date(EndTime) as EndTime,
-    VOName,
-    JobUsageRecord.ProbeName,
-    sum(Njobs) as Njobs,
-    sum(WallDuration) as WallDuration,
-    sum(CpuUserDuration) as CpuUserDuration,
-    sum(CpuSystemDuration) as CpuSystemDuration
-    from JobUsageRecord
+	(select
+		date(EndTime) as EndTime,
+		VOName,
+		ProbeName,
+		ResourceType,
+		sum(Njobs) as Njobs,
+		sum(WallDuration) as WallDuration,
+		sum(CpuUserDuration) as CpuUserDuration,
+		sum(CpuSystemDuration) as CpuSystemDuration
+		from JobUsageRecord
 		where CpuUserDuration is not null
-    group by VOName,ProbeName,date(EndTime)
+		group by VOName,ProbeName,ResourceType,date(EndTime)
 );
 ||
 alter table VOProbeSummary
-  add index index01(EndTime);
+	add index index01(EndTime);
 ||
 alter table VOProbeSummary
-  add index index02(VOName);
+	add index index02(VOName);
 ||
 alter table VOProbeSummary
-  add index index03(ProbeName);
+	add index index03(ProbeName);
+||
+alter table VOProbeSummary
+	add index index04(ResourceType);
 ||
 insert into HostDescriptionProbeSummary
-  (select
-    date(EndTime) as EndTime,
-    HostDescription,
-    JobUsageRecord.ProbeName,
-    sum(Njobs) as Njobs,
-    sum(WallDuration) as WallDuration,
-    sum(CpuUserDuration) as CpuUserDuration,
-    sum(CpuSystemDuration) as CpuSystemDuration
-    from JobUsageRecord
+	(select
+		date(EndTime) as EndTime,
+		HostDescription,
+		ProbeName,
+		ResourceType,
+		sum(Njobs) as Njobs,
+		sum(WallDuration) as WallDuration,
+		sum(CpuUserDuration) as CpuUserDuration,
+		sum(CpuSystemDuration) as CpuSystemDuration
+		from JobUsageRecord
 		where CpuUserDuration is not null
-    group by HostDescription,ProbeName,date(EndTime)
+		group by HostDescription,ProbeName,ResourceType,date(EndTime)
 );
 ||
 alter table HostDescriptionProbeSummary
-  add index index01(EndTime);
+	add index index01(EndTime);
 ||
 alter table HostDescriptionProbeSummary
-  add index index02(HostDescription);
+	add index index02(HostDescription);
 ||
 alter table HostDescriptionProbeSummary
-  add index index03(ProbeName);
+	add index index03(ProbeName);
+||
+alter table HostDescriptionProbeSummary
+	add index index04(ResourceType);
 ||
 drop trigger trigger01;
 ||
@@ -186,7 +207,8 @@ glr:begin
 
 	select count(*) into mycount from ProbeStatus
 		where ProbeStatus.ProbeName = new.ProbeName
-		and ProbeStatus.EndTime = str_to_date(date_format(new.ServerDate,'%Y-%c-%e %H:00:00'),'%Y-%c-%e %H:00:00');
+		and ProbeStatus.EndTime =
+		str_to_date(date_format(new.ServerDate,'%Y-%c-%e %H:00:00'),'%Y-%c-%e %H:00:00');
 	if mycount = 0 then
 		insert into ProbeStatus values(
 			str_to_date(date_format(new.ServerDate,'%Y-%c-%e %H:00:00'),'%Y-%c-%e %H:00:00'),
@@ -196,7 +218,8 @@ glr:begin
 			set
 				ProbeStatus.Njobs = ProbeStatus.Njobs + 1
 				where ProbeStatus.ProbeName = new.ProbeName
-				and ProbeStatus.EndTime = str_to_date(date_format(new.ServerDate,'%Y-%c-%e %H:00:00'),'%Y-%c-%e %H:00:00');
+				and ProbeStatus.EndTime =
+				str_to_date(date_format(new.ServerDate,'%Y-%c-%e %H:00:00'),'%Y-%c-%e %H:00:00');
 	end if;
 
 	--
@@ -205,9 +228,11 @@ glr:begin
 
 	select count(*) into mycount from ProbeSummary
 		where ProbeSummary.ProbeName = new.ProbeName
-		and ProbeSummary.EndTime = date(new.EndTime);
+		and ProbeSummary.EndTime = date(new.EndTime)
+		and ProbeSummary.ResourceType = new.ResourceType;
 	if mycount = 0 then
-		insert into ProbeSummary values(date(new.EndTime),new.ProbeName,new.SiteName,new.Njobs,new.WallDuration,new.CpuUserDuration,new.CpuSystemDuration);
+		insert into ProbeSummary values(date(new.EndTime),new.ProbeName,new.SiteName,new.ResourceType,
+			new.Njobs,new.WallDuration,new.CpuUserDuration,new.CpuSystemDuration);
 	elseif mycount > 0 then
 		update ProbeSummary
 			set
@@ -216,7 +241,8 @@ glr:begin
 				ProbeSummary.CpuUserDuration = ProbeSummary.CpuUserDuration + new.CpuUserDuration,
 				ProbeSummary.CpuSystemDuration = ProbeSummary.CpuSystemDuration + new.CpuSystemDuration
 				where ProbeSummary.ProbeName = new.ProbeName
-				and ProbeSummary.EndTime = date(new.EndTime);
+				and ProbeSummary.EndTime = date(new.EndTime)
+				and ProbeSummary.ResourceType = new.ResourceType;
 	end if;
 
 	--
@@ -227,9 +253,12 @@ glr:begin
 		where 
 		UserProbeSummary.CommonName = new.CommonName
 		and UserProbeSummary.ProbeName = new.ProbeName
-		and UserProbeSummary.EndTime = date(new.EndTime);
+		and UserProbeSummary.EndTime = date(new.EndTime)
+		and UserProbeSummary.ResourceType = new.ResourceType;
 	if mycount = 0 then
-		insert into UserProbeSummary values(date(new.EndTime),new.CommonName,new.ProbeName,new.Njobs,new.WallDuration,new.CpuUserDuration,new.CpuSystemDuration);
+		insert into UserProbeSummary values(date(new.EndTime),new.CommonName,
+			new.ProbeName,new.ResourceType,new.Njobs,new.WallDuration,
+			new.CpuUserDuration,new.CpuSystemDuration);
 	elseif mycount > 0 then
 		update UserProbeSummary
 			set
@@ -240,7 +269,8 @@ glr:begin
 				where 
 				UserProbeSummary.CommonName = new.CommonName
 				and UserProbeSummary.ProbeName = new.ProbeName
-				and UserProbeSummary.EndTime = date(new.EndTime);
+				and UserProbeSummary.EndTime = date(new.EndTime)
+				and UserProbeSummary.ResourceType = new.ResourceType;
 	end if;
 
 	--
@@ -250,9 +280,11 @@ glr:begin
 	select count(*) into mycount from VOProbeSummary
 		where VOProbeSummary.VOName = new.VOName
 		and VOProbeSummary.ProbeName = new.ProbeName
-		and VOProbeSummary.EndTime = date(new.EndTime);
+		and VOProbeSummary.EndTime = date(new.EndTime)
+		and VOProbeSummary.ResourceType = new.ResourceType;
 	if mycount = 0 then
-		insert into VOProbeSummary values(date(new.EndTime),new.VOName,new.ProbeName,new.Njobs,new.WallDuration,new.CpuUserDuration,new.CpuSystemDuration);
+		insert into VOProbeSummary values(date(new.EndTime),new.VOName,new.ProbeName,new.ResourceType,
+			new.Njobs,new.WallDuration,new.CpuUserDuration,new.CpuSystemDuration);
 	elseif mycount > 0 then
 		update VOProbeSummary
 			set
@@ -263,7 +295,8 @@ glr:begin
 				where 
 				VOProbeSummary.VOName = new.VOName
 				and VOProbeSummary.ProbeName = new.ProbeName
-				and VOProbeSummary.EndTime = date(new.EndTime);
+				and VOProbeSummary.EndTime = date(new.EndTime)
+				and VOProbeSummary.ResourceType = new.ResourceType;
 	end if;
 
 	--
@@ -273,20 +306,27 @@ glr:begin
 	select count(*) into mycount from HostDescriptionProbeSummary
 		where HostDescriptionProbeSummary.HostDescription = new.HostDescription
 		and HostDescriptionProbeSummary.ProbeName = new.ProbeName
-		and HostDescriptionProbeSummary.EndTime = date(new.EndTime);
+		and HostDescriptionProbeSummary.EndTime = date(new.EndTime)
+		and HostDescriptionProbeSummary.ResourceType = new.ResourceType;
 	if mycount = 0 then
-		insert into HostDescriptionProbeSummary values(date(new.EndTime),new.HostDescription,new.ProbeName,new.Njobs,new.WallDuration,new.CpuUserDuration,new.CpuSystemDuration);
+		insert into HostDescriptionProbeSummary values(date(new.EndTime),new.HostDescription,
+			new.ProbeName,new.ResourceType,new.Njobs,new.WallDuration,
+			new.CpuUserDuration,new.CpuSystemDuration);
 	elseif mycount > 0 then
 		update HostDescriptionProbeSummary
 			set
 				HostDescriptionProbeSummary.Njobs = HostDescriptionProbeSummary.Njobs + new.Njobs,
-				HostDescriptionProbeSummary.WallDuration = HostDescriptionProbeSummary.WallDuration + new.WallDuration,
-				HostDescriptionProbeSummary.CpuUserDuration = HostDescriptionProbeSummary.CpuUserDuration + new.CpuUserDuration,
-				HostDescriptionProbeSummary.CpuSystemDuration = HostDescriptionProbeSummary.CpuSystemDuration + new.CpuSystemDuration
+				HostDescriptionProbeSummary.WallDuration =
+					HostDescriptionProbeSummary.WallDuration + new.WallDuration,
+				HostDescriptionProbeSummary.CpuUserDuration =
+					HostDescriptionProbeSummary.CpuUserDuration + new.CpuUserDuration,
+				HostDescriptionProbeSummary.CpuSystemDuration =
+					HostDescriptionProbeSummary.CpuSystemDuration + new.CpuSystemDuration
 				where 
 				HostDescriptionProbeSummary.HostDescription = new.HostDescription
 				and HostDescriptionProbeSummary.ProbeName = new.ProbeName
-				and HostDescriptionProbeSummary.EndTime = date(new.EndTime);
+				and HostDescriptionProbeSummary.EndTime = date(new.EndTime)
+				and HostDescriptionProbeSummary.ResourceType = new.ResourceType;
 	end if;
 
 end;
@@ -294,6 +334,7 @@ end;
 show triggers;
 ||
 
-
-
-
+-- Local Variables:
+-- mode: sql
+-- eval: (sql-set-product 'mysql)
+-- End:
