d144a680eaa01728ea0c7c0ad57a0a7d11204040
greenc <greenc@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-04-24 18:50:40 +0000

    Conditional trigger drop routine to avoid Mysql errors.
    Trigger only attempts to update NodeSummary if it exists.
    
    
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@542 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- configuration/build-summary-tables.sql
+++ configuration/build-summary-tables.sql
@@ -178,12 +178,47 @@ alter table HostDescriptionProbeSummary
 alter table HostDescriptionProbeSummary
 	add index index04(ResourceType);
 ||
-drop trigger trigger01;
+drop procedure if exists conditional_trigger_drop
+||
+create procedure conditional_trigger_drop()
+begin
+
+	declare mycount int;
+
+	select count(*) into mycount from information_schema.triggers where
+		trigger_schema = Database()
+		and event_object_table = 'JobUsageRecord'
+		and trigger_name = 'trigger01';
+
+	if mycount > 0 then
+		drop trigger trigger01;
+  end if;
+end
+||
+call conditional_trigger_drop()
 ||
 create trigger trigger01 after insert on JobUsageRecord
 for each row
 glr:begin
 	declare mycount int;
+	declare startdate datetime;
+	declare enddate datetime;
+	declare node varchar(255);
+	declare myprobename varchar(255);
+	declare myresourcetype varchar(255);
+	declare mycpuusertime int default 0;
+	declare mycpusystemtime int default 0;
+	declare mycpucount int default 0;
+	declare myhostdescription varchar(255);
+	declare mybenchmarkscore int default 0;
+	declare divide int default 0;
+	declare counter int default 0;	
+	declare newdate datetime;
+	declare newcpusystemtime int default 0;
+	declare newcpuusertime int default 0;
+	declare numberofdays int default 0;
+	declare imax int default 0;
+
 	--
 	-- basic data checks
 	--
@@ -335,6 +370,62 @@ glr:begin
 				and HostDescriptionProbeSummary.ProbeName = new.ProbeName
 				and HostDescriptionProbeSummary.EndTime = date(new.EndTime)
 				and HostDescriptionProbeSummary.ResourceType = new.ResourceType;
+	end if;
+
+	--
+	-- NodeSummary
+	--
+
+	select count(*) into mycount from information_schema.tables where
+		table_schema = Database() and
+		table_name = 'NodeSummary';
+
+	if mycount > 0 then
+
+		set startdate = new.StartTime;
+		set enddate = new.EndTime;
+		set node = new.Host;
+		set myprobename = new.ProbeName;
+		set myresourcetype = new.ResourceType;
+		set mycpuusertime = new.CpuUserDuration;
+		set mycpusystemtime = new.CpuSystemDuration;
+		set myhostdescription = new.HostDescription;
+	
+		if myprobename = null then
+			set myprobename = 'Unknown';
+		end if;
+
+		begin
+			set mycpucount = 0;
+			set mybenchmarkscore = 0;
+			select BenchmarkScore,CPUCount into mybenchmarkscore,mycpucount from CPUInfo
+				where myhostdescription = CPUInfo.NodeName;
+		end;
+
+		set numberofdays = datediff(enddate,startdate);
+		set divide = numberofdays + 1;
+		set newcpusystemtime = mycpusystemtime / divide;
+		set newcpuusertime = mycpuusertime / divide;
+
+		if numberofdays = 0 then
+			call updatenodesummary(
+				date(enddate),node,myprobename,myresourcetype,
+				mycpusystemtime,mycpuusertime,mycpucount,myhostdescription,
+				mybenchmarkscore,extract(DAY from last_day(enddate)));
+		end if;
+
+		if numberofdays > 0 then
+			set imax = numberofdays + 1;
+			set counter = 0;
+			while counter < imax do
+				set newdate = adddate(startdate,counter);
+				call updatenodesummary(
+					date(newdate),node,myprobename,myresourcetype,
+					newcpusystemtime,newcpuusertime,mycpucount,
+					myhostdescription,mybenchmarkscore,extract(DAY from last_day(newdate)));
+				set counter = counter + 1;
+			end while;
+		end if;
 	end if;
 
 end;
