7c6c2761692bd9c3b0383e4f5065ff333073f367
pcanal <pcanal@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-05-25 14:24:35 +0000

    Add a field for the type of 'record'
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@607 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- services/net/sf/gratia/storage/DupRecord.java
+++ services/net/sf/gratia/storage/DupRecord.java
@@ -4,86 +4,97 @@ import java.util.*;
 
 public class DupRecord
 {
-		private int dupid;
-		private Date eventdate;
-		private String rawxml;
-		private String extraxml;
-		private String source;
-		private String error;
-		private int dbid;
+   private int dupid;
+   private Date eventdate;
+   private String rawxml;
+   private String extraxml;
+   private String source;
+   private String error;
+   private int dbid;
+   private String RecordType;
 
-		public DupRecord()
-		{
-		}
+   public DupRecord()
+   {
+   }
 
-		public int getdupid() 
-		{
-				return dupid;
-		}
+   public int getdupid()
+   {
+      return dupid;
+   }
 
-		public void setdupid(int value) 
-		{
-				dupid = value;
-		}
+   public void setdupid(int value)
+   {
+      dupid = value;
+   }
 
-		public Date geteventdate() 
-		{
-				return eventdate;
-		}
+   public Date geteventdate()
+   {
+      return eventdate;
+   }
 
-		public void seteventdate(Date value) 
-		{
-				eventdate = value;
-		}
+   public void seteventdate(Date value)
+   {
+      eventdate = value;
+   }
 
-		public String getrawxml() 
-		{
-				return rawxml;
-		}
+   public String getrawxml()
+   {
+      return rawxml;
+   }
 
-		public void setrawxml(String value) 
-		{
-				rawxml = value;
-		}
+   public void setrawxml(String value)
+   {
+      rawxml = value;
+   }
 
 
-		public String getextraxml() 
-		{
-				return extraxml;
-		}
+   public String getextraxml()
+   {
+      return extraxml;
+   }
 
-		public void setextraxml(String value) 
-		{
-				extraxml = value;
-		}
+   public void setextraxml(String value)
+   {
+      extraxml = value;
+   }
 
-		public void setsource(String value)
-		{
-				source = value;
-		}
+   public void setsource(String value)
+   {
+      source = value;
+   }
 
-		public String getsource()
-		{
-				return source;
-		}
+   public String getsource()
+   {
+      return source;
+   }
 
-		public void seterror(String value)
-		{
-				error = value;
-		}
+   public void seterror(String value)
+   {
+      error = value;
+   }
 
-		public String geterror()
-		{
-				return error;
-		}
+   public String geterror()
+   {
+      return error;
+   }
 
-		public void setdbid(int value)
-		{
-				dbid = value;
-		}
+   public void setdbid(int value)
+   {
+      dbid = value;
+   }
 
-		public int getdbid()
-		{
-				return dbid;
-		}
+   public int getdbid()
+   {
+      return dbid;
+   }
+
+   public void setRecordType(String value)
+   {
+      RecordType = value;
+   }
+
+   public String getRecordType()
+   {
+      return RecordType;
+   }
 }
