5d078c871d0fcfcdb5d66b91ac7cdfb3c1fe1630
greenc <greenc@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-05-29 21:04:17 +0000

    Make it work more betterer.
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@622 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- build-scripts/setup-jdk15.sh
+++ build-scripts/setup-jdk15.sh
@@ -1,12 +1,57 @@
+dropit=~greenc/bin/dropit
+have_dropit=1
 
-if [[ -z "$JAVA_HOME" ]] || [[ "$JAVA_HOME" == *jdk1.4* ]]; then
-  if [[ -n "$VDT_LOCATION" ]] && [[ -d "$VDT_LOCATION/jdk1.5" ]]; then
-    export JAVA_HOME="$VDT_LOCATION/jdk1.5"
-    PATH="$VDT_LOCATION/jdk1.5/bin:$PATH"
-    if [[ -n "$LD_LIBRARY_PATH" ]]; then
-      LD_LIBRARY_PATH="/opt/vdt/jdk1.5/jre/lib/i386:/opt/vdt/jdk1.5/jre/lib/i386/server:/opt/vdt/jdk1.5/jre/lib/i386/client:$LD_LIBRARY_PATH"
-    else
-      export LD_LIBRARY_PATH="/opt/vdt/jdk1.5/jre/lib/i386:/opt/vdt/jdk1.5/jre/lib/i386/server:/opt/vdt/jdk1.5/jre/lib/i386/client"
-    fi
+function add_to_path() {
+	[[ -n "$dropit" ]] || dropit=`type -p dropit`
+  OPTIND=1 # must reset
+  local dropit_args="-s -e" # default
+  while getopts :sfep: OPT; do
+    case $OPT in
+      s)
+	;;
+      f)
+	local dropit_f_arg="-f"
+	;;
+      e)
+	;;
+      p)
+	local dropit_p_arg="-p"
+	local dropit_path="$OPTARG"
+	;;
+    esac
+  done
+  shift $[ OPTIND - 1 ]
+  if have_dropit; then
+#    set -x
+    printf "%s" `$dropit $dropit_args $dropit_f_arg $dropit_p_arg "$dropit_path" "$@"`
+#    set +x
+  else
+    # Real simple: no removal of duplicates
+    [[ -z "$dropit_path" ]] && dropit_path="$PATH"
+    for dropit_frag in "$@"; do
+      if [ -n "$dropit_f_arg" ]; then
+        dropit_path="${dropit_frag}:${dropit_path}"
+      else
+        dropit_path="${dropit_path}:${dropit_frag}"
+      fi
+    done
+    printf "%s" "${dropit_path}"
   fi
+}
+
+if [[ -z "$JAVA_HOME" ]] && [[ -d ~greenc/java/jdk1.5.0_11 ]]; then
+  export JAVA_HOME=~greenc/java/jdk1.5.0_11
+  export LD_LIBRARY_PATH=`add_to_path -p "$LD_LIBRARY_PATH" -f "$JAVA_HOME/lib/i386/"{,"server/","client/"}`
+  PATH=`add_to_path -f "$JAVA_HOME/bin"`
 fi
+
+# Tell [X]Emacs that we want to be in sh-mode[bash] even though we don't
+# have an interpreter line.
+#
+# Upon loading the file, [X]Emacs will ask for permission to evaluate 
+# the form below. Satisfy yourself of the safety of this form 
+# (eg C-h f sh-set-shell) before confirming execution.
+### Local Variables:
+### mode: sh
+### eval: (sh-set-shell "bash" t nil)
+### End:
