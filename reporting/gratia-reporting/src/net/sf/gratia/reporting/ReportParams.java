2133ec31ee889e08f79c6f104330768276975404
penelopec <penelopec@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-06-01 18:09:41 +0000

    *** empty log message ***
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@646 c85a5f45-a5f3-44fc-b02c-7891c381c519

---
+++ reporting/gratia-reporting/src/net/sf/gratia/reporting/ReportParams.java
@@ -0,0 +1,119 @@
+package net.sf.gratia.reporting;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class ReportParams
+{
+
+   // Converts a date from mm/dd/yyyy hh:mm:ss am where any one of the 
+   // time fields are missing to a java date object
+   public static Date convertStringToDate(String dateToConvert)
+   {
+      String working = dateToConvert;
+      int pos = working.indexOf("/");
+      Integer month, day, year;
+      if (pos >= 0)
+      {
+         month = new Integer(working.substring(0, working.indexOf("/")));
+         working = working.substring(working.indexOf("/") + 1);
+         day = new Integer(working.substring(0, working.indexOf("/")));
+         working = working.substring(working.indexOf("/") + 1);
+         year = new Integer(working.substring(0, 4));
+      }
+      else
+      {
+         pos = working.indexOf("-");
+         if (pos <= 2)
+         {
+            month = new Integer(working.substring(0, working.indexOf("-")));
+            working = working.substring(working.indexOf("-") + 1);
+            day = new Integer(working.substring(0, working.indexOf("-")));
+            working = working.substring(working.indexOf("-") + 1);
+            year = new Integer(working.substring(0, 4));
+         }
+         else
+         {
+            year = new Integer(working.substring(0, working.indexOf("-")));
+            working = working.substring(working.indexOf("-") + 1);
+            month = new Integer(working.substring(0, working.indexOf("-")));
+            working = working.substring(working.indexOf("-") + 1);
+            day = new Integer(working.substring(0, 2));
+         }
+      }
+
+      Integer hour = 0;
+      Integer minute = 0;
+      Integer second = 0;
+
+      if (working.indexOf(" ") > -1)
+      {
+         working = working.substring(working.indexOf(" ") + 1);
+         hour = new Integer(working.substring(0, working.indexOf(":")));
+         working = working.substring(working.indexOf(":") + 1);
+         if (working.indexOf(":") > -1)
+         {
+            if (working.substring(0, working.indexOf(":")) != "00")
+               minute = new Integer(working.substring(0, working.indexOf(":")));
+            working = working.substring(working.indexOf(":"));
+            //second = working.substring(working.indexOf(":") +1);
+         }
+         else
+         {
+            // System.out.println("ReportParams: working:" + working + ":date to convert:" + dateToConvert);
+            // In the case of 'selected start date', the start date is in the format that we display, so convert accordingly
+            if (working.indexOf(" ") > -1)
+            {
+               if (working.substring(0, working.indexOf(" ")) != "00")
+                  minute = new Integer(working.substring(0, working.indexOf(" ")));
+               //second = 0;
+               working = working.substring(working.indexOf(" ") + 1);
+               if (working == "PM")
+                  hour = hour + 12;
+            }
+            else
+            {
+               minute = 0;
+               second = 0;
+            }
+         }
+      }
+      return new Date(new GregorianCalendar(year, month - 1, day, hour, minute, second).getTimeInMillis());
+   }
+
+   //	Converts a java date object to string format with zero padding, as required by the SQL
+   public static String convertDateToString(Date dateToConvert)
+   {
+      DateFormat f = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+      return f.format(dateToConvert);
+   }
+
+   //	Reset the hour to the end or begins of the day
+   public static Date resetHours(Date dateToReset, int isStartDate)
+   {
+      int hour, minute, second;
+      if (isStartDate == 1)
+      {
+         hour = 0;
+         minute = 0;
+         second = 0;
+      }
+      else
+      {
+         // If it is the end date, make hours 23, minutes and seconds 59
+         hour = 23;
+         minute = 59;
+         second = 59;
+      }
+      GregorianCalendar cal = new GregorianCalendar();
+      cal.setTime(dateToReset);
+      cal.set(Calendar.HOUR_OF_DAY, hour);
+      cal.set(Calendar.MINUTE, minute);
+      cal.set(Calendar.SECOND, second);
+      dateToReset.setTime(cal.getTimeInMillis());
+      return dateToReset;
+   }
+
+}
---
+++ reporting/gratia-reporting/src/net/sf/gratia/reporting/ReportingConfiguration.java
@@ -0,0 +1,128 @@
+package net.sf.gratia.reporting;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.Properties;
+
+//import java.util.logging.Level;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+//import org.eclipse.birt.report.engine.api.EngineConfig;
+//import org.eclipse.birt.report.engine.api.ReportEngine;
+//import org.eclipse.birt.report.model.api.DesignEngine;
+//import org.eclipse.birt.report.model.api.SessionHandle;
+
+import net.sf.gratia.reporting.exceptions.InvalidConfigurationException;
+
+public class ReportingConfiguration 
+{
+	private String _configLoaded = null;
+	private String _reportsFolder = null;
+	private String _engineHome = null;
+	private String _webappHome = null;
+	private String _databaseURL = null;
+	private String _databaseUser = null;
+	private String _databasePassword = null;
+	private String _reportsMenuConfig = null;
+
+
+	//private static EngineConfig _reportEngineConfig = null;
+	//private static ReportEngine _reportEngine = null;
+	
+	//private static SessionHandle _designSession = null;
+	
+	public String getDatabasePassword() {
+		return _databasePassword;
+	}
+
+	public String getDatabaseURL() {
+		return _databaseURL;
+	}
+
+	public String getDatabaseUser() {
+		return _databaseUser;
+	}
+
+	public String getEngineHome() {
+		return _engineHome;
+	}
+
+	public String getReportsFolder() {
+		return _reportsFolder;
+	}
+
+	public String getWebappHome() {
+		return _webappHome;
+	}
+
+	public String getReportsMenuConfig() {
+		return _reportsMenuConfig;
+	}		
+	
+	/*
+	public static EngineConfig getReportEngineConfig() {
+		if(_reportEngineConfig == null)
+		{
+			_reportEngineConfig = new EngineConfig();
+			_reportEngineConfig.setEngineHome( getEngineHome() );
+			_reportEngineConfig.setLogConfig("", Level.SEVERE);
+		}
+		
+		return _reportEngineConfig;
+	}	
+	
+	public static ReportEngine getReportEngine() {
+		if(_reportEngine == null)
+		{
+			_reportEngine = new ReportEngine(getReportEngineConfig());		    
+		}
+		
+		return _reportEngine;
+	}	
+	
+	public static SessionHandle getDesignSession() {
+		if(_designSession == null)
+		{
+			_designSession = DesignEngine.newSession( null );
+		}
+		
+		return _designSession;
+	}
+	*/
+	
+	public void loadReportingConfiguration(javax.servlet.http.HttpServletRequest request)
+	{		
+		
+		Properties p = net.sf.gratia.util.Configuration.getProperties();
+
+		// The special key 'configLoaded' is set when the configuration has been already loaded
+		   if (_configLoaded == null)
+		   {			
+			try
+			{
+				String catalinaHome = net.sf.gratia.util.Configuration.getCatalinaHome();
+				
+				_reportsFolder = (catalinaHome + "/webapps/" + p.getProperty("service.reporting.reports.folder") + "/");
+				_engineHome = (catalinaHome + "/webapps/" + p.getProperty("service.reporting.engine.home") + "/");
+				_webappHome = (catalinaHome + "/webapps/" + p.getProperty("service.reporting.webapp.home") + "/");		        	
+			 
+				_databaseURL =  p.getProperty("service.mysql.url");
+				_databaseUser = p.getProperty("service.reporting.user");
+				_databasePassword = p.getProperty("service.reporting.password");
+				_reportsMenuConfig = p.getProperty("service.reporting.menuconfig");			        	       	
+		   
+			    				
+	 // Set a flag indicating the configuration has been loaded, so subsequent calls will not load again
+		   		 _configLoaded = "1";
+		   	}
+
+		   	catch(Exception ignore)
+			{
+			}
+		   }		
+	}
+	
+}
