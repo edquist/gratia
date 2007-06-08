bccff6070f5c4f01e9d1c38e7d127dabe0ee566b
pcanal <pcanal@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-06-08 11:03:18 +0000

    RolesTable -> Role
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@701 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- common/configuration/build-roles-table.sql
+++ common/configuration/build-roles-table.sql
@@ -1,7 +1,7 @@
 delimiter |
-delete from RolesTable
+delete from Role
 |
-insert into RolesTable(role,subtitle,whereclause) values('GratiaGlobalAdmin','GratiaGlobalAdmin','')
+insert into Role(role,subtitle,whereclause) values('GratiaGlobalAdmin','GratiaGlobalAdmin','')
 |
-insert into RolesTable(role,subtitle,whereclause) values('GratiaUser','GratiaUser',"Everything")
+insert into Role(role,subtitle,whereclause) values('GratiaUser','GratiaUser',"Everything")
 |
