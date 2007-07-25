9544f4f421310065ea7f429bdd5ccf4b225c1df7
pcanal <pcanal@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-07-25 20:35:57 +0000

    Separate the history and old vector so that we can compare them separately (and hence avoid delete __all__ the history directories)
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@911 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- collector/gratia-services/net/sf/gratia/services/HistoryReaper.java
+++ collector/gratia-services/net/sf/gratia/services/HistoryReaper.java
@@ -7,127 +7,139 @@ import java.text.*;
 
 public class HistoryReaper
 {
-		public String driver;
-		public String url;
-		public String user;
-		public String password;
+   public String driver;
+   public String url;
+   public String user;
+   public String password;
 
-		Connection connection;
-		Statement statement;
-		ResultSet resultSet;
+   Connection connection;
+   Statement statement;
+   ResultSet resultSet;
 
-		String command;
+   String command;
 
-		Properties p;
-		
-		XP xp = new XP();
+   Properties p;
 
-		String filenames[] = new String[0];
+   XP xp = new XP();
 
-		public HistoryReaper()
-		{
-				Logging.log("HistoryReaper: Starting");
+   String filenames[] = new String[0];
 
-				p = net.sf.gratia.services.Configuration.getProperties();
-				driver = p.getProperty("service.mysql.driver");
-				url = p.getProperty("service.mysql.url");
-				user = p.getProperty("service.mysql.user");
-				password = p.getProperty("service.mysql.password");
-				openConnection();
-				getDirectories();
-		}
+   public HistoryReaper()
+   {
+      Logging.log("HistoryReaper: Starting");
 
-		public void openConnection()
-		{
-				try
-						{
-								Class.forName(driver).newInstance();
-								connection = null;
-								connection = DriverManager.getConnection(url,user,password);
-						}
-				catch (Exception e)
-						{
-						}
-		}
+      p = net.sf.gratia.services.Configuration.getProperties();
+      driver = p.getProperty("service.mysql.driver");
+      url = p.getProperty("service.mysql.url");
+      user = p.getProperty("service.mysql.user");
+      password = p.getProperty("service.mysql.password");
+      openConnection();
+      getDirectories();
+   }
 
-		public void deleteDirectory(String path)
-		{
-				String files[] = xp.getFileList(path);
+   public void openConnection()
+   {
+      try
+      {
+         Class.forName(driver).newInstance();
+         connection = null;
+         connection = DriverManager.getConnection(url, user, password);
+      }
+      catch (Exception e)
+      {
+      }
+   }
 
-				for (int i = 0; i < files.length; i++)
-						{
-								try
-										{
-												File file = new File(files[i]);
-												file.delete();
-										}
-								catch (Exception ignore)
-										{
-										}
-						}
-				try
-						{
-								File file = new File(path);
-								file.delete();
-								Logging.log("HistoryReaper: Deleted Directory: " + path + ":Files: " + files.length);
-						}
-				catch (Exception ignore)
-						{
-						}
-		}
+   public void deleteDirectory(String path)
+   {
+      String files[] = xp.getFileList(path);
 
-		public void getDirectories()
-		{
-				int i = 0;
-				Vector vector = new Vector();
-				String path = System.getProperties().getProperty("catalina.home") + "/gratia/data";
-				path = xp.replaceAll(path,"\\","/");
-				Logging.log("HistoryReaper: Path: " + path);
-				String temp[] = xp.getDirectoryList(path);
-				for (i = 0; i < temp.length; i++)
-						{
-								String directory = temp[i];
-								if ((directory.indexOf("history") > -1) || (directory.indexOf("old") > -1))
-										vector.add(temp[i]);
-						}
-				Logging.log("HistoryReaper: Directories To Process: " + vector.size());
-				//
-				// figure out which directories to delete
-				//
-				java.util.Date now = new java.util.Date();
-				long nowmilli = now.getTime();
-				long historymilli = Long.parseLong(p.getProperty("maintain.history.log"));
-				historymilli = historymilli * 24 * 60 * 60 * 1000;
-				java.util.Date oldest = new java.util.Date(nowmilli - historymilli);
-				Calendar beginning = Calendar.getInstance();
-				beginning.setTime(oldest);
-				beginning.set(Calendar.SECOND,0);
-				beginning.set(Calendar.MINUTE,0);
-				SimpleDateFormat format = new SimpleDateFormat("yyyyMMddhh");
+      for (int i = 0; i < files.length; i++)
+      {
+         try
+         {
+            File file = new File(files[i]);
+            file.delete();
+         }
+         catch (Exception ignore)
+         {
+         }
+      }
+      try
+      {
+         File file = new File(path);
+         file.delete();
+         Logging.log("HistoryReaper: Deleted Directory: " + path + ":Files: " + files.length);
+      }
+      catch (Exception ignore)
+      {
+      }
+   }
 
-				String beginningHistory = path + "/history" + format.format(beginning.getTime());
-				Logging.log("HistoryReaper: First History Directory To Process: " + beginningHistory);
+   public void getDirectories()
+   {
+      int i = 0;
+      Vector history = new Vector();
+      Vector old = new Vector();
+      String path = System.getProperties().getProperty("catalina.home") + "/gratia/data";
+      path = xp.replaceAll(path, "\\", "/");
+      Logging.log("HistoryReaper: Path: " + path);
+      String temp[] = xp.getDirectoryList(path);
+      for (i = 0; i < temp.length; i++)
+      {
+         String directory = temp[i];
+         if (directory.indexOf("history") > -1) 
+         {
+            history.add(temp[i]);
+         }
+         else if (directory.indexOf("old") > -1)
+         {
+            old.add(temp[i]);
+         }
+      }
+      Logging.log("HistoryReaper: 'old' Directories To Process: " + history.size());
+      Logging.log("HistoryReaper: 'history' Directories To Process: " + old.size());
+      //
+      // figure out which directories to delete
+      //
+      java.util.Date now = new java.util.Date();
+      long nowmilli = now.getTime();
+      long historymilli = Long.parseLong(p.getProperty("maintain.history.log"));
+      historymilli = historymilli * 24 * 60 * 60 * 1000;
+      java.util.Date oldest = new java.util.Date(nowmilli - historymilli);
+      Calendar beginning = Calendar.getInstance();
+      beginning.setTime(oldest);
+      beginning.set(Calendar.SECOND, 0);
+      beginning.set(Calendar.MINUTE, 0);
+      SimpleDateFormat format = new SimpleDateFormat("yyyyMMddhh");
 
-				String beginningOld = path + "/old" + format.format(beginning.getTime());
-				Logging.log("HistoryReaper: First Old Directory To Process: " + beginningOld);
+      String beginningHistory = path + "/history" + format.format(beginning.getTime());
+      Logging.log("HistoryReaper: First History Directory To Process: " + beginningHistory);
 
-				//
-				// now - screen/delete older directories
-				//
-				for (i = 0; i < vector.size(); i++)
-						{
-								String directory = (String) vector.elementAt(i);
-								if (directory.compareTo(beginningHistory) < 0)
-										{
-												Logging.log("HistoryReaper: Deleting Directory: " + directory);
-												deleteDirectory(directory);
-										}
-								if (directory.compareTo(beginningOld) < 0)
-										{
-												Logging.log("HistoryReaper: Deleting Directory: " + directory);
-												deleteDirectory(directory);
-										}
-						}
-		}
+      String beginningOld = path + "/old" + format.format(beginning.getTime());
+      Logging.log("HistoryReaper: First Old Directory To Process: " + beginningOld);
+
+      //
+      // now - screen/delete older directories
+      //
+      for (i = 0; i < history.size(); i++)
+      {
+         String directory = (String)history.elementAt(i);
+         if (directory.compareTo(beginningHistory) < 0)
+         {
+            Logging.log("HistoryReaper: Deleting Directory: " + directory);
+            deleteDirectory(directory);
+         }
+      }
+      for (i = 0; i < old.size(); i++)
+      {
+         String directory = (String)old.elementAt(i);
+         if (directory.compareTo(beginningOld) < 0)
+         {
+            Logging.log("HistoryReaper: Deleting Directory: " + directory);
+            deleteDirectory(directory);
+         }
+      }
+   }
 
 }
