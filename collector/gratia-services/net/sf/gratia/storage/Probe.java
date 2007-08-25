33fdaae4bb7ec998cb2f219febc090ea8f353f61
pcanal <pcanal@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-08-25 04:25:37 +0000

    Add a object connection to Site.  Add nConnections and nDuplicates.
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@988 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- collector/gratia-services/net/sf/gratia/storage/Probe.java
+++ collector/gratia-services/net/sf/gratia/storage/Probe.java
@@ -2,123 +2,163 @@ package net.sf.gratia.storage;
 
 public class Probe
 {
-        private int probeid;
-		private int siteid;
-		private String probename;
-		private DateElement currenttime;
-		private int active;
-		private int reporthh;
-		private int reportmm;
-		private String status;
-		private int nRecords;
+    private int probeid;
 
-		public Probe()
-		{
-		}
+    private Site site;
 
-		public int getsiteid() 
-		{
-				return siteid;
-		}
+    private String probename;
+    private DateElement currenttime;
+    private int active;
+    private int reporthh;
+    private int reportmm;
+    private String status;
+    private int nRecords;
+    private int nConnections;
+    private int nDuplicates;
 
-		public void setsiteid(int siteid) 
-		{
-				this.siteid = siteid;
-		}
+    public Probe()
+    {
+        site = null;
+        nRecords = 0;
+        nConnections = 0;
+        nDuplicates = 0;
+    }
 
-		public int getprobeid() 
-		{
-				return probeid;
-		}
+    public Probe(String name)
+    {
+        probename = name;
+        site = null;
+    }
+    
+    public int getsiteid() 
+    {
+        if (site != null) return site.getsiteid();
+        else return 0;
+    }
+    
+//     public void setsiteid(int siteid) 
+//     {
+//         this.siteid = siteid;
+//     }
 
-		public void setprobeid(int probeid) 
-		{
-				this.probeid = probeid;
-		}
-
-		public String getprobename() 
-		{
-				return probename;
-		}
-
-		public void setprobename(String name) 
-		{
-				probename = name;
-		}
-	
-		public DateElement getcurrenttime()
-		{
-				return currenttime;
-		}
-
-		public void setcurrenttime(DateElement time)
-		{
-				this.currenttime = time;
-		}
-
-		public int getreporthh()
-		{
-				return reporthh;
-		}
-
-		public void setreporthh(int value)
-		{
-				this.reporthh = value;
-		}
-
-		public int getreportmm()
-		{
-				return reportmm;
-		}
-
-		public void setreportmm(int value)
-		{
-				this.reportmm = value;
-		}
-
-		public int getactive()
-		{
-				return active;
-		}
-
-		public void setactive(int value)
-		{
-				this.active = value;
-		}
-
-		public void setstatus(String value)
-		{
-				this.status = value;
-		}
-
-		public String getstatus()
-		{
-				return status;
-		}
-
-		public int getnRecords()
-		{
-				return nRecords;
-		}
-
-		public void setnRecords(int value)
-		{
-				this.nRecords = value;
-		}
-
-		public String toString() 
-		{
-				String output = 
-						"Probe: " + 
-						" siteid: " + siteid +
-						" probeid: " + probeid +
-						" probename: " + probename +
-						" currenttime: " + currenttime +
-						" active: " + active +
-						" reporthh: " + reporthh +
-						" reportmm: " + reportmm +
-						" status: " + status +
-						" nRecords: " + nRecords;
+    public Site getsite() { return site; }
+    public void setsite(Site s) { site = s; }
+    
+    public int getprobeid() 
+    {
+        return probeid;
+    }
+    
+    public void setprobeid(int probeid) 
+    {
+        this.probeid = probeid;
+    }
+    
+    public String getprobename() 
+    {
+        return probename;
+    }
+    
+    public void setprobename(String name) 
+    {
+        probename = name;
+    }
+    
+    public DateElement getcurrenttime()
+    {
+        return currenttime;
+    }
+    
+    public void setcurrenttime(DateElement time)
+    {
+        this.currenttime = time;
+    }
+    
+    public int getreporthh()
+    {
+        return reporthh;
+    }
+    
+    public void setreporthh(int value)
+    {
+        this.reporthh = value;
+    }
+    
+    public int getreportmm()
+    {
+        return reportmm;
+    }
+    
+    public void setreportmm(int value)
+    {
+        this.reportmm = value;
+    }
+    
+    public int getactive()
+    {
+        return active;
+    }
+    
+    public void setactive(int value)
+    {
+        this.active = value;
+    }
+    
+    public void setstatus(String value)
+    {
+        this.status = value;
+    }
+    
+    public String getstatus()
+    {
+        return status;
+    }
+    
+    public int getnRecords()
+    {
+        return nRecords;
+    }
+    
+    public void setnRecords(int value)
+    {
+        this.nRecords = value;
+    }
+    
+    public int getnConnections()
+    {
+        return nConnections;
+    }
+    
+    public void setnConnections(int value)
+    {
+        this.nConnections = value;
+    }
+    
+    public int getnDuplicates()
+    {
+        return nDuplicates;
+    }
+    
+    public void setnDuplicates(int value)
+    {
+        this.nDuplicates = value;
+    }
+    
+    public String toString() 
+    {
+        String output = 
+            "Probe: " + 
+            " siteid: " + getsiteid() +
+            " probeid: " + probeid +
+            " probename: " + probename +
+            " currenttime: " + currenttime +
+            " active: " + active +
+            " reporthh: " + reporthh +
+            " reportmm: " + reportmm +
+            " status: " + status +
+            " nRecords: " + nRecords +
+            " nConnections: " + nConnections +
+            " nDuplicates: " + nDuplicates;
         return output;
     }
 }
