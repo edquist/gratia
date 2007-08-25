f9ed4ad27c00bf20a037bc7546376904db719e65
pcanal <pcanal@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-08-25 04:24:14 +0000

    Fix tab. Add new constructor
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@987 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- collector/gratia-services/net/sf/gratia/storage/Site.java
+++ collector/gratia-services/net/sf/gratia/storage/Site.java
@@ -2,31 +2,36 @@ package net.sf.gratia.storage;
 
 public class Site
 {
-		private int siteid;
-		private String siteName;
+    private int siteid;
+    private String siteName;
 	
-		public Site()
-		{
-		}
+    public Site()
+    {
+    }
 
-		public int getsiteid() {
-				return siteid;
-		}
+    public Site(String name)
+    {
+        siteName = name;
+    }
 
-		public void setsiteid(int id) {
-				this.siteid = id;
-		}
-
-		public String getsiteName() {
-				return siteName;
-		}
-
-		public void setsiteName(String name) {
-            siteName = name;
-		}
-	
-		public String toString() {
-				String output = "Site: " + "siteid: " + siteid + " SiteName: " + siteName;
+    public int getsiteid() {
+        return siteid;
+    }
+    
+    public void setsiteid(int id) {
+        this.siteid = id;
+    }
+    
+    public String getsiteName() {
+        return siteName;
+    }
+    
+    public void setsiteName(String name) {
+        siteName = name;
+    }
+    
+    public String toString() {
+        String output = "Site: " + "siteid: " + siteid + " SiteName: " + siteName;
         return output;
     }
 }
