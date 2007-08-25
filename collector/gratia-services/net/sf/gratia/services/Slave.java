c8c947a52a5bb3e44a930250077ac2225ec27c09
pcanal <pcanal@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-08-25 06:08:34 +0000

    removes tabs
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@993 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- collector/gratia-services/net/sf/gratia/services/Slave.java
+++ collector/gratia-services/net/sf/gratia/services/Slave.java
@@ -8,103 +8,103 @@ import java.net.*;
 
 public class Slave extends Thread
 {
-		public static int count = 0;
-		public int mycount = 0;
-
-		//
-		// internal stuff
-		//
-
-		Properties p;
-		Socket socket;
-		byte[] buffer = new byte[4096];
-  
-
-		public Slave(Socket socket)
-		{
-				p = Configuration.getProperties();
-				this.socket = socket;
-		}
-
-		public void mysleep(long time)
-		{
-				try
-						{
-								Thread.sleep(time);
-						}
-				catch (Exception ignore)
-						{
-						}
-		}
+    public static int count = 0;
+    public int mycount = 0;
+    
+    //
+    // internal stuff
+    //
+    
+    Properties p;
+    Socket socket;
+    byte[] buffer = new byte[4096];
+    
+    
+    public Slave(Socket socket)
+    {
+	p = Configuration.getProperties();
+	this.socket = socket;
+    }
+    
+    public void mysleep(long time)
+    {
+	try
+	    {
+		Thread.sleep(time);
+	    }
+	catch (Exception ignore)
+	    {
+	    }
+    }
+    
+    public void run()
+    {
+	try
+	    {
+		String xml = readSocket();
+		if (xml == null)
+		    {
+			writeSocket("Error: No Data");
+			closeSocket();
+			return;
+		    }
+		if (xml.length() == 0)
+		    {
+			writeSocket("Error: No Data");
+			closeSocket();
+			return;
+		    }
+		JMSProxy proxy = (JMSProxy) Naming.lookup(p.getProperty("service.rmi.rmilookup") +
+							  p.getProperty("service.rmi.service"));
+		proxy.update(xml);
+		writeSocket("OK");
+		closeSocket();
+		Slave.count++;
+		Logging.info("Slave: Record: " + Slave.count);
+	    }
+	catch (Exception e)
+	    {
+		e.printStackTrace();
+	    }
+    }
+    
+    //
+    // socket handling
+    //
+    
+    public String readSocket()
+    {
+	try
+	    {
+		DataInputStream input = new DataInputStream(socket.getInputStream());
 		
-		public void run()
-		{
-				try
-						{
-								String xml = readSocket();
-								if (xml == null)
-										{
-												writeSocket("Error: No Data");
-												closeSocket();
-												return;
-										}
-								if (xml.length() == 0)
-										{
-												writeSocket("Error: No Data");
-												closeSocket();
-												return;
-										}
-								JMSProxy proxy = (JMSProxy) Naming.lookup(p.getProperty("service.rmi.rmilookup") +
-																													p.getProperty("service.rmi.service"));
-								proxy.update(xml);
-								writeSocket("OK");
-								closeSocket();
-								Slave.count++;
-								Logging.info("Slave: Record: " + Slave.count);
-						}
-				catch (Exception e)
-						{
-								e.printStackTrace();
-						}
-		}
-
-		//
-		// socket handling
-		//
-
-		public String readSocket()
-		{
-				try
-						{
-								DataInputStream input = new DataInputStream(socket.getInputStream());
-
-								int count = input.read(buffer);
-								String request = new String(buffer,0,count);
-								return request;
-						}
-				catch (Exception e)
-						{
-								return null;
-						}
-		}
-  
-		public void writeSocket(String message) throws Exception
-		{
-				if (message == null)
-						return;
-				DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-				output.write(message.getBytes(),0,message.getBytes().length);
-		}
-
-		public void closeSocket()
-		{
-				try
-						{
-								socket.close();
-						}
-				catch (Exception ignore)
-						{
-						}
-		}
-
+		int count = input.read(buffer);
+		String request = new String(buffer,0,count);
+		return request;
+	    }
+	catch (Exception e)
+	    {
+		return null;
+	    }
+    }
+    
+    public void writeSocket(String message) throws Exception
+    {
+	if (message == null)
+	    return;
+	DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+	output.write(message.getBytes(),0,message.getBytes().length);
+    }
+    
+    public void closeSocket()
+    {
+	try
+	    {
+		socket.close();
+	    }
+	catch (Exception ignore)
+	    {
+	    }
+    }
+    
 }
