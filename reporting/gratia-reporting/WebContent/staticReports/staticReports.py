70e223047da73b3ee843ee8f6bf5551586635ed7
greenc <greenc@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-11-20 19:33:14 +0000

    Remove tabs; and handle some common exceptions.
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@1464 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- reporting/gratia-reporting/WebContent/staticReports/staticReports.py
+++ reporting/gratia-reporting/WebContent/staticReports/staticReports.py
@@ -13,24 +13,26 @@ def walkTree(node):
 
 def showNode(node, reportsFolder, staticFolder, birtURL):
     if node.nodeName == "reportItem":
-	# get the attributes.
-	attrs = node.attributes
+        # get the attributes.
+        attrs = node.attributes
         for attrName in attrs.keys():
             if attrName == 'link':
-	        reportsFolder = reportsFolder + '/'
-		urlQuery = attrs.get(attrName).nodeValue
-		i = urlQuery.index(']') + 1
-		s1 = urlQuery[i:]
-		i = s1.index('.') + 1
-		reportFile = os.path.join(staticFolder, s1[:i] + 'pdf')
-		## print reportFile		
-	        callURL = birtURL + '/' + attrs.get(attrName).nodeValue.replace('[ReportsFolder]', reportsFolder)
-	        ## print ('URL \n%s\n' % (callURL))
-		f = urllib.urlopen(callURL)
-		w = open(reportFile, 'w')
-		w.write(f.read())
-		w.close
-		
+                reportsFolder = reportsFolder + '/'
+        urlQuery = attrs.get(attrName).nodeValue
+        i = urlQuery.index(']') + 1
+        s1 = urlQuery[i:]
+        i = s1.index('.') + 1
+        reportFile = os.path.join(staticFolder, s1[:i] + 'pdf')
+        ## print reportFile     
+        callURL = birtURL + '/' + attrs.get(attrName).nodeValue.replace('[ReportsFolder]', reportsFolder)
+        ## print ('URL \n%s\n' % (callURL))
+        try:
+            f = urllib.urlopen(callURL)
+            w = open(reportFile, 'w')
+            w.write(f.read())
+            w.close
+        except IOError, e:
+            sys.exit(1)
 
 def parseFile(inFileName, reportsFolder, staticFolder, birtURL): 
     doc = minidom.parse(inFileName)
@@ -42,7 +44,7 @@ def main():
     args = sys.argv[1:]
     if len(args) != 2:
         print 'usage: ./staticReports.py catalina.home reportsURL'
-        sys.exit(-1)
+        sys.exit(1)
     config = ConfigParser()
     catalinaHome = args[0]
     birtURL = args[1]
@@ -52,18 +54,21 @@ def main():
     exists = os.path.exists(reportsFolder)
     if exists != 1:
         print ('Reports folder %s does not exist' %(reportsFolder))
-	sys.exit(-1)
+        sys.exit(1)
     staticConfig = os.path.join(catalinaHome, 'webapps', config.get( 'gratia', 'service.reporting.staticreports'))
     exists = os.path.exists(staticConfig)
     if exists != 1:
         print ('Static Reports configuration file \n %s \n does not exist' %(staticConfig))
-	sys.exit(-1)
+        sys.exit(1)
     staticFolder = os.path.join(catalinaHome, 'webapps', config.get( 'gratia', 'service.reporting.static.folder'))
+    try:
+        os.mkdir(staticFolder)
+    except OSError, e:
+        pass
     exists = os.path.exists(staticFolder)
     if exists != 1:
-        print ('Static Reports folder \n    %s \ndoes not exist' %(staticFolder))
-	os.mkdir(staticFolder)
-       
+        print ('Static Reports folder \n    %s \ndoes not exist and cannot be made' %(staticFolder))
+        sys.exit(1)
     parseFile(staticConfig, reportsFolder, staticFolder, birtURL)
 
 if __name__ == '__main__':
