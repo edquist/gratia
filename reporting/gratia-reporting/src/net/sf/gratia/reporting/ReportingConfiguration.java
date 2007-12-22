78c3aed8c736018d8aa3c2fba3c3dd10f61ede34
penelopec <penelopec@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2007-12-22 07:41:31 +0000

    Fixed static report file separator for windows
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@1537 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- reporting/gratia-reporting/src/net/sf/gratia/reporting/ReportingConfiguration.java
+++ reporting/gratia-reporting/src/net/sf/gratia/reporting/ReportingConfiguration.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.Properties;
 import javax.servlet.http.HttpServletRequest;
 
-public class ReportingConfiguration 
+public class ReportingConfiguration
 {
 	HttpServletRequest request = null;
 	Properties p = net.sf.gratia.util.Configuration.getProperties();
@@ -19,7 +19,7 @@ public class ReportingConfiguration
 	private String _staticFolder = null;
 	private String _staticFolderPath = null;
 	private String _statReportsConfig = null;
-	
+
 	public String getDatabasePassword() {
 		return _databasePassword;
 	}
@@ -39,7 +39,7 @@ public class ReportingConfiguration
 	public String getReportsMenuConfig() {
 		return _reportsMenuConfig;
 	}
-	
+
 	public String getConfigLoaded() {
 		return _configLoaded;
 	}
@@ -47,15 +47,15 @@ public class ReportingConfiguration
 	public String getCsvHome() {
 		return _csvHome;
 	}
-	
+
 	public String getReportingVersion() {
 		return _reportingVersion;
 	}
-	
+
 	public String getStaticFolder() {
 		return _staticFolder;
 	}
-	
+
 	public String getStaticReportsConfig() {
 		return _statReportsConfig;
 	}
@@ -65,44 +65,44 @@ public class ReportingConfiguration
 	}
 
 	public void loadReportingConfiguration(javax.servlet.http.HttpServletRequest request)
-	{		
+	{
 		this.request = request;
 		Properties p = net.sf.gratia.util.Configuration.getProperties();
 
 		// The special key 'configLoaded' is set when the configuration has been already loaded
-		   if (_configLoaded == null)
-		   {			
+		if (_configLoaded == null)
+		{
 			try
 			{
 				String webappsHome =  System.getProperty("catalina.home") + File.separatorChar+ "webapps" + File.separatorChar;
-				
+
 				_reportsFolder     = (webappsHome + p.getProperty("service.reporting.reports.folder").replace("/", File.separator) + File.separatorChar);
 				_reportsMenuConfig = (webappsHome + p.getProperty("service.reporting.menuconfig").replace("/", File.separator));
 				_statReportsConfig = (webappsHome + p.getProperty("service.reporting.staticreports").replace("/", File.separator));
-				_staticFolderPath  = (webappsHome + p.getProperty("service.reporting.static.folder") + File.separatorChar);				
+				_staticFolderPath  = (webappsHome + p.getProperty("service.reporting.static.folder").replace("/", File.separator) + File.separatorChar);
 				_staticFolder      = p.getProperty("service.reporting.static.folder");
 
 				if (_staticFolder.charAt(_staticFolder.length()-1) != '/')
 					_staticFolder = _staticFolder + "/";
 				if (_staticFolder.charAt(0) != '/')
 					_staticFolder = "/" + _staticFolder;
-				
+
 				_csvHome           = (webappsHome + "birt_csv_temp" + File.separatorChar);
-				
+
 				_databaseURL       = p.getProperty("service.mysql.url");
 				_databaseUser      = p.getProperty("service.reporting.user");
-				_databasePassword  = p.getProperty("service.reporting.password");				
+				_databasePassword  = p.getProperty("service.reporting.password");
 				_staticFolder      = p.getProperty("service.reporting.static.folder");
 
 				_reportingVersion  = p.getProperty("gratia.reporting.version");
-		   	
-	 // Set a flag indicating the configuration has been loaded, so subsequent calls will not load again
-		   		 _configLoaded = "1";
 
-		   	} catch (Exception e) {
+	 // Set a flag indicating the configuration has been loaded, so subsequent calls will not load again
+				 _configLoaded = "1";
+
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		   }		
+		}
 	}
 
 }
