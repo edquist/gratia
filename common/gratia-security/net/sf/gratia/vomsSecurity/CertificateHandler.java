f309b44aa4eacd86d755675902decb107ca9d0e2
penelopec <penelopec@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2008-03-25 16:06:18 +0000

    Reads service properties file & voms-servers for vo info
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@1849 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- common/gratia-security/net/sf/gratia/vomsSecurity/CertificateHandler.java
+++ common/gratia-security/net/sf/gratia/vomsSecurity/CertificateHandler.java
@@ -3,13 +3,8 @@ package net.sf.gratia.vomsSecurity;
 import java.io.*;
 import java.net.URL;
 import java.util.*;
-import java.text.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-//import java.security.cert.*;
 
-//import org.bouncycastle.asn1.*;
-//import org.bouncycastle.asn1.util.*;
+import javax.servlet.http.*;
 
 import java.security.cert.X509Certificate;
 import org.glite.security.SecurityContext;
@@ -18,204 +13,273 @@ import org.glite.security.util.DNImpl;
 import org.glite.security.voms.service.admin.VOMSAdmin;
 import org.glite.security.voms.service.admin.VOMSAdminServiceLocator;
 
-import net.sf.gratia.util.*;
-import java.sql.*;
-
+//@SuppressWarnings("unchecked")
 public class CertificateHandler
 {
-	HttpServletRequest request = null;
-	X509Certificate[] certs;
-	//
-	// database related
-	//
-	String driver = "";
-	String url    = "";
+	/*
+	 *	Only VOs listed in the service-configuration.properties have administrative access.
+	 *	The user is prompt to select an appropriate VO from this list.
+	 *	Upon selection, the voms server for the specified VO is contacted to get the Roles
+	 *	the user roles.
+	 *	The voms server link is listed in the "voms-servers" file, which is setup
+	 *	by the administrator of the gratia system.
+	 *	To grant access, the user selected role must match one of the roles listed in the
+	 *	service-configuration.properties file.
+	 */
 
-	String password = "";
-	Connection connection;
-	Statement statement;
-	ResultSet resultSet;
+	private String _vomsServerFile;
+	private boolean _vomsServersLoaded = false;
 
-	//---- certificate information -----
-	String user   = null;
-	String dn     = null;
-	String ca     = null;
-	String vo     = null;
-	String voServer = null;
-	String[] VoNodes = {"VDT"};
+	//---- certificate information ----
+	private String dn        = null;
+	private String ca        = null;
+	private String voServer  = null;
 
-	// --- VOMS information ---
-	String[] roles  = null;
-	String[] rolesFound  = null;
-	String[] groups = null;
+	//---- VOMS information ----
+	private String[] voFQANlist = null;		// list of FQAN with admin access to compare with
+	private String[] voList     = null;		// list of VOs to display for user selection
+	private String[] rolesVOMS  = null;		// list of VOMS roles to display for user selection
+	private String[] groupsVOMS = null;		// list of VOMS groups to display for user selection
 
-	String voSelect = null;
-	String voConnect = null;
+	private String voSelect  = null;		// User selected VO
+	private String voConnect = null;		// User selected VO connection
 
-	private String _configLoaded = null;
+	private Object[] voVOMSNameList = null;
+	private Object[] voVOMSUrlList  = null;
+
+	private boolean _configLoaded    = false;
 	private String _secureConnection = null;
-	private String _openConnection = null;
-	private String _dbConnection = null;
+	private String _openConnection   = null;
+	private String _dbConnection     = null;
+
+	//---------------------------------------------------
+	public CertificateHandler(HttpServletRequest request)
+	{
+		// ---- load the service-configuration.properties ----
+		loadServiceConfigurationProperties();
+
+		// ---- reading certificate for dn/ca for VOMS call ----
+		SecurityContext sc = new SecurityContext();
+		SecurityContext.setCurrentContext(sc);
+		
+		X509Certificate[] certs = (X509Certificate[]) request.getAttribute("javax.servlet.request.X509Certificate");
+
+		if (certs == null)
+			return;
+
+		sc.setClientCertChain(certs);
+
+		if (sc.getClientName () != null)
+			sc.setClientName (new DNImpl(sc.getClientName ()).getX500());
+
+		if (sc.getIssuerName () != null)
+			sc.setIssuerName (new DNImpl(sc.getIssuerName ()).getX500());
+
+		X509Certificate cs = sc.getClientCert();
+		this.dn = ((DNImpl) DNHandler.getSubject(cs)).getX500();
+		this.ca = ((DNImpl) DNHandler.getIssuer(cs)).getX500();
+
+		// ---- setting up to call VOMS (not sure how much is needed)
+		//		this needs to be set some other way (TRUSTED_CA on VDT dist ----
+		System.setProperty("sslCAFiles","/etc/grid-security/certificates/*.0");
+		// this may be ok
+		System.setProperty("axis.socketSecureFactory","org.glite.security.trustmanager.axis.AXISSocketFactory");
+		// these needs to be set some other way (I think there is a properties file)
+		System.setProperty("sslKey","/etc/grid-security/http/httpkey.pem");
+		System.setProperty("sslCertfile","/etc/grid-security/http/httpcert.pem");
+
+	}
 
 	// ----------------------------------
-	public String getSecureConnection() {
-		return _secureConnection;
-	}
-	   
-	// ----------------------------------
-	public String getOpenConnection() {
-		return _openConnection;
-	}
-	   
-	// ----------------------------------
-	public String getdbConnection() {
-		return _dbConnection;
-	}
-	   
-	// ----------------------------------
-	public void loadCertificateConfiguration(HttpServletRequest request)
+	public void loadServiceConfigurationProperties()
 	{
-		this.request = request;
 		Properties p = net.sf.gratia.util.Configuration.getProperties();
+		String vos;
+		String voName = "";
+		TreeSet tempVOs = new TreeSet();	// sorted arrayList with unique names for the voList
+		List tempFQANs  = new ArrayList();	// FQAN list from service properties file
 
 		// The flag 'configLoaded' is set when the configuration has been already loaded
-		if (_configLoaded == null)
+		if (!_configLoaded)
 		{
 			try
 			{
-				String webappsHome =  System.getProperty("catalina.home") + "/" + "webapps" + "/";
-
+				_vomsServerFile = net.sf.gratia.util.Configuration.getConfigurationPath() + "/" + p.getProperty("service.voms.connections");
 				_secureConnection = p.getProperty("service.secure.connection");
 				_openConnection   = p.getProperty("service.open.connection");
 				_dbConnection     = p.getProperty("service.mysql.url");
 
-				// Set a flag indicating the configuration has been loaded, so subsequent calls will not load again
-				_configLoaded = "1";
+				boolean foundAdmin = true;
+				int admid = 0;
+				String admin_id = "service.admin.identity.";
+				while(foundAdmin)
+				{
+					vos = p.getProperty(admin_id + admid);
 
-			} catch (Exception e) {
+					if (vos != null)
+					{
+						vos.trim();
+						tempFQANs.add(vos);
+		/*
+		 * Extract the node name from the identity, example of identity:
+		 * 	     /gratia-vo1/Role=VO-Admin
+		 * we want to extract gratia-vo1 to populate the pull down menu.
+		 */
+						String[] VoFullStr = vos.split ("/");
+						voName = VoFullStr[1];
+						if (voName != null)
+						{
+							tempVOs.add(voName);
+						}
+						admid += 1;
+					}
+					else
+					{
+						foundAdmin = false;
+					}
+				}
+				// Convert the ArrayLists to string arrays
+				voList     = (String[])tempVOs.toArray(new String[tempVOs.size()]);
+				voFQANlist = (String[])tempFQANs.toArray(new String[tempFQANs.size()]);
+
+				// Set a flag indicating the configuration has been loaded, so subsequent calls will not load again
+				_configLoaded = true;
+
+			} 
+			catch (Exception e) 
+			{
 				e.printStackTrace();
 			}
 		}
 	}
 
-	//---------------------------------------------------
-	public CertificateHandler(HttpServletRequest request)
-	{
-		this.request = request;
-
-		System.out.println("### CertificateHandler### In CertificateHandler class");
-		// ---- reading certificate for dn/ca for VOMS call ---
-		SecurityContext sc = new SecurityContext();
-		SecurityContext.setCurrentContext(sc);
-
-		System.out.println("### CertificateHandler### reading certificate");
-		certs = (X509Certificate[]) request.getAttribute("javax.servlet.request.X509Certificate");
-
-		if (certs==null) {
-			return;
-		} else {
-			System.out.println("### CertificateHandler### got certificate");
-		}
-
-		sc.setClientCertChain(certs);
-
-		if (sc.getClientName () != null)
-			sc.setClientName (new DNImpl (sc.getClientName ()).getX500());
-
-		if (sc.getIssuerName () != null)
-			sc.setIssuerName (new DNImpl(sc.getIssuerName ()).getX500());
-
-		System.out.println("### CertificateHandler### getting names ");
-		X509Certificate cs=sc.getClientCert();
-		this.dn=((DNImpl) DNHandler.getSubject(cs)).getX500();
-		this.ca=((DNImpl) DNHandler.getIssuer(cs)).getX500();
-
-		System.out.println("### CertificateHandler### DN: "+dn);
-		System.out.println("### CertificateHandler### CA: "+ca);
-
-		// ---- setting up to call VOMS (not sure how much is needed ---
-		// this needs to be set some other way (TRUSTED_CA on VDT dist /
-				System.setProperty("sslCAFiles","/etc/grid-security/certificates/*.0");
-		// this may be ok
-				System.setProperty("axis.socketSecureFactory","org.glite.security.trustmanager.axis.AXISSocketFactory");
-		// these needs to be set some other way (I think there is a properties file)
-				System.setProperty("sslKey","/etc/grid-security/http/httpkey.pem");
-				System.setProperty("sslCertfile","/etc/grid-security/http/httpcert.pem");
-
-		// ---- calling VOMS ---
-
-		/**
-		System.out.println("### CertificateHandler:  calling connect to vo ### ");
-		String vonam = "oiv_test1";
-		voConnect = vonam.trim();
-
-
-		String retcode = connectVOname(voConnect);
-		System.out.println("### CertificateHandler:  after calling connect to vo, code =  " + retcode +  "### ");
-		**/
-
-	}
 	// ----------------------------------
+	public String loadVOServers(String voFile)
+	{
+		if (!_vomsServersLoaded)
+		{
+			List voVOMSNam = new ArrayList();	// list of VO names from the voms-server file
+			List voVOMSUrl = new ArrayList();	// list of URLs for VO from the voms-server file
 
+			String InStrLine;	// Input line
+			String StrLine;		// Line to parse after trimming
+
+			try
+			{
+				BufferedReader inFile = new BufferedReader (new FileReader(voFile) );
+
+				while ( (InStrLine = inFile.readLine()) != null)
+				{
+					StrLine = InStrLine.trim();
+					if (StrLine.length() == 0) continue;	// Skip empty lines
+					if (StrLine.startsWith("#")) continue;
+
+					// Line is not a comment, parsing it using token to break it into fields
+					String[] VoInfo = StrLine.split ("=");
+
+					String  voNam = VoInfo[0].trim();
+					String  voUrl = VoInfo[1].trim();
+					voVOMSNam.add(voNam);
+					voVOMSUrl.add(voUrl);
+				}
+			} catch (IOException e) {
+				String msg = "CERTIFICATE HANDLER message: <br>" + e.getMessage();
+				//System.out.println(msg);
+				return msg;
+			}
+
+			// Convert now to arrays
+			voVOMSNameList = voVOMSNam.toArray();
+			voVOMSUrlList  = voVOMSUrl.toArray();
+			_vomsServersLoaded = true;
+			return "";
+		}
+		else
+			return "";
+	}
+
+	// ----------------------------------
 	public String connectVOname(String voname)
 	{
+//		Read voms-servers file: the file resides in the tomcat/gratia area
+		String fileStatus = loadVOServers(_vomsServerFile);
+		if (fileStatus.length() > 1 )
+			return fileStatus;
+
 		voConnect = voname.trim();
-		//String VomsLocation = "https://gratiax34.fnal.gov:8443/voms/" + voConnect + "/services/VOMSAdmin";
 
-		String VomsLocation = getVoServer(voConnect);	// still hardcoded for now
+		String VomsLocation = getVoVOMSserver(voConnect);
 
-		System.out.println("### CertificateHandler:connectVOname ###Locating SELECTED VOMS Server"+VomsLocation);
-		System.out.println("### CertificateHandler:connectVOname, dn " + this.dn + " ==== ");
-		System.out.println("### CertificateHandler:connectVOname, ca " + this.ca + " ==== ");
-		//return "Connected";
 		// ---- calling VOMS ---
-		/** **/
 		final VOMSAdmin stub;
-		try {
+		try
+		{
 			// other test VOMS servers
-			//String VomsLocation = "https://gratiax34.fnal.gov:8443/voms/" + voConnect + "/services/VOMSAdmin";
+			// String VomsLocation = "https://gratiax34.fnal.gov:8443/voms/" + voConnect + "/services/VOMSAdmin";
 
-			System.out.println("### CertificateHandler: connectVOname : ###Locating VOMS Server: "+VomsLocation);
 			final VOMSAdminServiceLocator locator = new VOMSAdminServiceLocator();
 			stub = locator.getVOMSAdmin(new URL(VomsLocation));
 
-			System.out.println("### CertificateHandler: connectVOname : ###  Getting roles from  VOMS Server");
-			roles  = stub.listRoles( dn, ca);
-			this.rolesFound = roles;
-			System.out.println("### CertificateHandler: connectVOname : ###  Getting groups from  VOMS Server");
-			groups = stub.listGroups(dn,ca);
-		} catch (Exception e) {
-			String msg = e.getMessage();
-			System.out.println("### CertificateHandler: connectVOname : ###  error:\n\t\t"+msg);
-			//e.printStackTrace();
-			return "Connection Error";
+			this.rolesVOMS  = stub.listRoles(dn, ca);
+			this.groupsVOMS = stub.listGroups(dn,ca);
 		}
-		System.out.println("### CertificateHandler: connectVOname : ### Roles:");
-		for( int i=0; i < roles.length; i++) {
-			System.out.println("### ====== role =    "+roles[i]);
-			System.out.println("### ====== roleFound =    "+rolesFound[i]);
+		catch (Exception e)
+		{
+			String msg = "CERTIFICATE HANDLER message: <br>" + e.getMessage();
+			//System.out.println(msg);
+			return msg;
 		}
-		System.out.println("### CertificateHandler: connectVOname : ### Groups:");
-		for( int i=0; i < groups.length; i++) {
-			System.out.println("### ====== group =     "+groups[i]);
-		}
-		return "Connected";   /** **/
+
+		return "";
+	}
+
+	// ----------------------------------
+	public String getSecureConnection() {
+		return this._secureConnection;
+	}
+
+	// ----------------------------------
+	public String getOpenConnection() {
+		return this._openConnection;
+	}
+
+	// ----------------------------------
+	public String getDBconnection() {
+		return this._dbConnection;
 	}
 
 	//---------------------------------------------------
-	public String getVoServer(String voCon)
+	// The information comes from the voms-servers configuration file under
+	//	the tomcat/gratia area
+
+	public String getVoVOMSserver(String voCon)
 	{
-		this.voServer = "https://gratiax34.fnal.gov:8443/voms/" + voCon + "/services/VOMSAdmin";
+		String  conUrl = null;
+
+		for (int j=0; j < voVOMSNameList.length; j++)
+		{
+			if (voVOMSNameList[j].toString().toUpperCase().trim().equals(voCon.toUpperCase().trim()))
+			{
+				conUrl = voVOMSUrlList[j].toString().trim();
+			}
+		}
+		this.voServer = conUrl + "/services/VOMSAdmin";
 		return this.voServer;
 	}
 
-    //---------------------------------------------------
-	public String[] getVoNodes()
+	//---------------------------------------------------
+	public String[] getVOlist()
 	{
-		String[] VoNodesInit = {"VDT","gratia-vo1","gratia-vo2","cms","cdf"};
-		this.VoNodes = VoNodesInit;
-		return this.VoNodes;
+		return this.voList;
 	}
+
+	//---------------------------------------------------
+
+	public String[] getFQANlist()
+	{
+		return this.voFQANlist;
+	}
+
 	//---------------------------------------------------
 	public String getDN()
 	{
@@ -225,28 +289,29 @@ public class CertificateHandler
 	}
 
 	//---------------------------------------------------
-	public String getName()
+	public String getDNname()
 	{
-
-		String dnString="";
+		String dnString = "";
 		String[] subwords;
 
-		if (this.dn == null){
+		if (this.dn == null)
+		{
 			return "GratiaUserName";
-		} else {
+		}
+		else
+		{
 			dnString = this.dn;
-			//System.out.println("### CertificateHandler:getName: Extracting name from dn " + dnString);
 			String[] dnWords = dnString.split ("/");
-			for (int i=0; i < dnWords.length; i++){
-			//System.out.println (dnWords[i]);
+
+			for (int i=0; i < dnWords.length; i++)
+			{
 				subwords = dnWords[i].split ("=");
-				for (int ij=0; ij < subwords.length; ij++){
-					//System.out.println ("sub= " + subwords[ij]);
-					if (subwords[ij].equals("UID") ) {
-						String userName=subwords[ij+1];
-						//System.out.println("----- got userName = " + userName);
-						this.user = userName;
-						return this.user;
+				for (int ij=0; ij < subwords.length; ij++)
+				{
+					if (subwords[ij].equals("UID"))
+					{
+						String userName = subwords[ij+1];
+						return userName;
 					}
 				}
 			}
@@ -254,33 +319,33 @@ public class CertificateHandler
 		return "GratiaUserName";
 	}
 
-    // ----------------------------------
-	public String[] getRole()
+	// ----------------------------------
+	public String[] getVOMSroles()
 	{
-		String[] initRoles ={"GratiaUserRole"};
+		String[] initRoles = {"GratiaUserRole"};
+		String[] userRoles = {""};
+		String[] RolesStr = {"NoRoles"};
 
-		if (this.rolesFound== null)
+		if (this.rolesVOMS == null)
 			return initRoles;
-		return this.rolesFound;
-		/*if (this.roles == null)
-				return "GratiaUserRole";
-		return "Test";*/
+
+		if (this.rolesVOMS.length <= 0)
+		{
+			// System.out.println("CERTIFICATE HANDLER Message:  NO roles found ");
+			return RolesStr;
+		}
+
+		return this.rolesVOMS;
 	}
 
 	// ----------------------------------
-	public String getGroups()
+	public String [] getVOMSgroups()
 	{
-		if (this.groups == null)
-			return "GratiaUserGrp";
-		return "Test";
-	}
+		String[] initGroup = {"GratiaUserGrp"};
 
-	// ----------------------------------
-	public String getVO()
-	{
-		if (this.vo == null)
-			 return "Unknown";
-		return vo;
+		if (this.groupsVOMS == null)
+			return initGroup;
+		return this.groupsVOMS;
 	}
 
 	// ----------------------------------
@@ -293,97 +358,7 @@ public class CertificateHandler
 	// ----------------------------------
 	public String getVOname()
 	{
-
-		return voSelect;
+		return this.voSelect;
 	}
 
-	// ----------------------------------
-	public String getSubtitle()
-	{
-		String dq = "'";
-		String command = "select * from Role where role = " + dq + getRole() + dq;
-		String subtitle = "Test Subtitle";
-		openConnection();
-		try
-		{
-			statement = connection.prepareStatement(command);
-			resultSet = statement.executeQuery(command);
-			while(resultSet.next())
-			{
-				subtitle = resultSet.getString("subtitle");
-			}
-			resultSet.close();
-			statement.close();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-			closeConnection();
-			return subtitle;
-		}
-
-		return subtitle;
-	}
-
-	public void dump()
-	{
-		StringBuffer buffer = new StringBuffer();
-		String cr = "\n";
-		int i;
-		return; // wanted to terminate as this whole thing servers no purpose
-
-	}
-
-	public void append(String string)
-	{
-		String path = System.getProperties().getProperty("catalina.home");
-		path = path + "/logs/gratia-certificates.log";
-		try
-		{
-			RandomAccessFile output = new RandomAccessFile(path,"rw");
-			output.seek(output.length());
-			output.write(string.getBytes());
-			output.close();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	public void openConnection()
-	{
-		try
-		{
-			Properties p = Configuration.getProperties();
-			driver = p.getProperty("service.mysql.driver");
-			url = p.getProperty("service.mysql.url");
-			user = p.getProperty("service.mysql.user");
-			password = p.getProperty("service.mysql.password");
-		}
-		catch (Exception ignore)
-		{
-		}
-		try
-		{
-			Class.forName(driver).newInstance();
-			connection = DriverManager.getConnection(url,user,password);
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	public void closeConnection()
-	{
-		try
-		{
-			connection.close();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
 }
