962d6249528a2b98e39a899497bc70832ea15b1e
penelopec <penelopec@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2008-03-26 21:34:10 +0000

    Added non-secure link stopGratiaDatabaseUpdateThreads.html
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@1857 c85a5f45-a5f3-44fc-b02c-7891c381c519

---
+++ collector/gratia-administration/net/sf/gratia/administration/StopDatabase.java
@@ -0,0 +1,142 @@
+package net.sf.gratia.administration;
+
+import net.sf.gratia.util.XP;
+import net.sf.gratia.util.Configuration;
+import net.sf.gratia.util.Logging;
+import net.sf.gratia.services.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.StringTokenizer;
+import java.util.Properties;
+import java.util.Hashtable;
+import java.util.Enumeration;
+import java.util.Vector;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+import java.util.regex.*;
+import java.text.*;
+import java.rmi.*;
+import org.apache.tools.bzip2.*;
+import com.ice.tar.*;
+
+public class StopDatabase extends HttpServlet 
+{
+	XP xp = new XP();
+	//
+	// database related
+	//
+	String driver = "";
+	String url = "";
+	String user = "";
+	String password = "";
+	Connection connection;
+	Statement statement;
+	ResultSet resultSet;
+	//
+	// processing related
+	//
+	String html = "";
+	String row = "";
+	StringBuffer buffer = new StringBuffer();
+	//
+	// globals
+	//
+	HttpServletRequest request;
+	HttpServletResponse response;
+	boolean initialized = false;
+	Properties props;
+	Properties p;
+	String message = null;
+	//
+	// support
+	//
+	String dq = "\"";
+	String comma = ",";
+	String cr = "\n";
+
+	public JMSProxy proxy = null;
+
+	//
+	// statics for recovery thread
+	//
+
+	public static String status = "";
+	public static long skipped = 0;
+	public static long processed = 0;
+	public static long errors = 0;
+	public static boolean replayall = false;
+
+	public void initialize()
+	{
+		p = net.sf.gratia.util.Configuration.getProperties();
+		try
+		{
+			proxy = (JMSProxy) Naming.lookup(p.getProperty("service.rmi.rmilookup") +
+					p.getProperty("service.rmi.service"));
+		}
+		catch (Exception e)
+		{
+			Logging.warning(xp.parseException(e));
+		}
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		initialize();
+		this.request = request;
+		this.response = response;
+		stopDatabaseUpdateThreads();
+
+		setup();
+		process();
+		response.setContentType("text/html");
+		response.setHeader("Cache-Control", "no-cache"); // HTTP 1.1
+		response.setHeader("Pragma", "no-cache"); // HTTP 1.0
+		PrintWriter writer = response.getWriter();
+		writer.write(html);
+		writer.flush();
+		writer.close();
+	}
+
+	public void setup()
+	{
+		html = xp.get(request.getRealPath("/") + "systemadministration.html");
+	}
+
+	public void process()
+	{
+		String status = "Active";
+
+		html = xp.replaceAll(html,"#status#", StopDatabase.status);
+		html = xp.replaceAll(html,"#processed#","" + StopDatabase.processed);
+		html = xp.replaceAll(html,"#skipped#","" + StopDatabase.skipped);
+
+		try
+		{
+			boolean flag = proxy.databaseUpdateThreadsActive();
+			if (flag)
+				status = "Alive";
+			else
+				status = "Stopped";
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		html = xp.replaceAll(html,"#threadstatus#",status);
+	}
+
+	public void stopDatabaseUpdateThreads()
+	{
+		try
+		{
+			proxy.stopDatabaseUpdateThreads();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
