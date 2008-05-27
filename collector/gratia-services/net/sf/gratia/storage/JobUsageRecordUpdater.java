fe0815ec23bfa64a27dab119576873a2f4e4b4f2
greenc <greenc@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2008-05-27 19:56:58 +0000

    Give up trying to guess which CN is the "right" one: record them all.
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@2102 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- collector/gratia-services/net/sf/gratia/storage/JobUsageRecordUpdater.java
+++ collector/gratia-services/net/sf/gratia/storage/JobUsageRecordUpdater.java
@@ -279,37 +279,19 @@ public abstract class JobUsageRecordUpdater implements RecordUpdater
         private String getCNFromDN(String subjectName) {
             String[] subjectNameFields = subjectName.split("[,/]");
             String userName = null;
-            int cnToIgnore = 0;
-            int cnSeen = 0;
             for (int i = 0; i < subjectNameFields.length; ++i) {
                 String caseFieldValue = subjectNameFields[i].trim();
                 String fieldValue = subjectNameFields[i].toLowerCase().trim();
-
-                if (fieldValue.equals("ou=robots")) {
-                    // This is for cron, we need the 3rd CNs:
-                    //     /DC=gov/DC=fnal/O=Fermilab/OU=Robots/CN=fermigrid0.fnal.gov/CN=cron/CN=Keith Chadwick/CN=UID:chadwick
-                    cnToIgnore = 2;
-                }
                 if (fieldValue.startsWith("cn=")) {
-                    // Raw DN: Grab *first* CN clause for CommonName (thank you, KCA)
-                    if (cnSeen < cnToIgnore) {
-                        String name = caseFieldValue.substring(3);
-                        Utils.GratiaDebug("Skipping CommonName " + name + 
-                                         " found in subject " + subjectName);
-                        if ( (cnSeen+1) == cnToIgnore && ! name.equals("cron") ) {
-                            Utils.GratiaInfo("Warning: 2nd CN of a robot DN is expected to be 'cron',"+
-                                             " we found " + name +
-                                             " in subject " + subjectName); 
-                        }
-                    } else if (userName != null) {
-                        Utils.GratiaDebug("Extra CommonName " + caseFieldValue.substring(3) +
-                                         " found in subject " + subjectName + ": IGNORED" );
+                    if (userName == null) {
+                        userName = "/" + caseFieldValue;
                     } else {
-                        userName = caseFieldValue.substring(3);
-                        Utils.GratiaDebug("Extracted a Username from subject:  " + userName);
+                        userName = userName + "/" + caseFieldValue;
                     }
-                    cnSeen = cnSeen + 1;
                 }
+            }
+            if (userName != null) {
+                Utils.GratiaDebug("Extracted username(s) from subject:  " + userName);
             }
             return userName;
         }
