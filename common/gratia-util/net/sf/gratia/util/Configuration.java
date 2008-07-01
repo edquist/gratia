cb47fc86b5583fb9e26e859757d88aaecbfb75d0
greenc <greenc@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2008-07-01 23:08:38 +0000

    White space.
    XP is static.
    
    New methods getJobUsageSummaryPath() and getNodeSummaryPath().
    
    
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@2241 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- common/gratia-util/net/sf/gratia/util/Configuration.java
+++ common/gratia-util/net/sf/gratia/util/Configuration.java
@@ -7,82 +7,89 @@ import net.sf.gratia.util.Logging;
 import java.util.*;
 import java.io.*;
 
-public class Configuration
-{
+public class Configuration {
 
-		public static XP xp = new XP();
+    public static String getCatalinaHome()
+    {
+        return System.getProperty("catalina.home");
+    }
 
-		public static String getCatalinaHome()
-		{
-				return System.getProperty("catalina.home");
-		}
+    public static String getConfigurationPath()
+    {
+        String path = System.getProperty("catalina.home");
+        path = path + "/gratia";
+        return path;
+    }
 
-		public static String getConfigurationPath()
-		{
-				String path = System.getProperty("catalina.home");
-				path = path + "/gratia";
-				return path;
-		}
+    public static String getHibernatePath()
+    {
+        return getConfigurationPath() + "/" + "hibernate.cfg.xml";
+    }
 
-		public static String getHibernatePath()
-		{
-				return getConfigurationPath() + "/" + "hibernate.cfg.xml";
-		}
+    public static String getGratiaHbmPath()
+    {
+        return getConfigurationPath() + "/" + "Gratia.hbm.xml";
+    }
 
-		public static String getGratiaHbmPath()
-		{
-				return getConfigurationPath() + "/" + "Gratia.hbm.xml";
-		}
+    public static String getJobUsagePath()
+    {
+        return getConfigurationPath() + "/" + "JobUsage.hbm.xml";
+    }
 
-      public static String getJobUsagePath()
-      {
-         return getConfigurationPath() + "/" + "JobUsage.hbm.xml";
-      }
+    public static String getJobUsageSummaryPath()
+    {
+        return getConfigurationPath() + "/" + "JobUsageSummary.hbm.xml";
+    }
 
-      public static String getMetricRecordPath()
-      {
-         return getConfigurationPath() + "/" + "MetricRecord.hbm.xml";
-      }
+    public static String getNodeSummaryPath()
+    {
+        return getConfigurationPath() + "/" + "NodeSummary.hbm.xml";
+    }
 
-      public static String getJMSPath()
-		{
-				return getConfigurationPath() + "/" + "a3servers.xml";
-		}
+    public static String getMetricRecordPath()
+    {
+        return getConfigurationPath() + "/" + "MetricRecord.hbm.xml";
+    }
 
-		public static Properties getProperties()
-		{
-				Properties p = new Properties();
-				try
-						{
-								p.load(new FileInputStream(new File(getConfigurationPath() + "/" + "service-configuration.properties")));
-						}
-				catch (Exception e)
-						{
-								Logging.log("Error Loading: " + getConfigurationPath() + "/" + "service-configuration.properties");
-								e.printStackTrace();
-						}
-				return p;
-		}
+    public static String getJMSPath()
+    {
+        return getConfigurationPath() + "/" + "a3servers.xml";
+    }
 
-		public static String getAccountingTablePath()
-		{
-				return getConfigurationPath() + "/" + "accounting-table";
-		}
+    public static Properties getProperties()
+    {
+        Properties p = new Properties();
+        try
+            {
+                p.load(new FileInputStream(new File(getConfigurationPath() + "/" + "service-configuration.properties")));
+            }
+        catch (Exception e)
+            {
+                Logging.log("Error Loading: " + getConfigurationPath() + "/" + "service-configuration.properties");
+                e.printStackTrace();
+            }
+        return p;
+    }
 
-		public static synchronized Hashtable getAccountingTable()
-		{
-				Hashtable accountingTable = (Hashtable) xp.getObject(getAccountingTablePath());
-				if (accountingTable == null)
-						{
-								accountingTable = new Hashtable();
-								saveAccountingTable(accountingTable);
-						}
-				return accountingTable;
-		}
+    public static String getAccountingTablePath()
+    {
+        return getConfigurationPath() + "/" + "accounting-table";
+    }
 
-		public static synchronized void saveAccountingTable(Hashtable table)
-		{
-				xp.putObject(getAccountingTablePath(),table);
-		}
+    public static synchronized Hashtable getAccountingTable()
+    {
+        Hashtable accountingTable = (Hashtable) XP.getObject(getAccountingTablePath());
+        if (accountingTable == null)
+            {
+                accountingTable = new Hashtable();
+                saveAccountingTable(accountingTable);
+            }
+        return accountingTable;
+    }
+
+    public static synchronized void saveAccountingTable(Hashtable table)
+    {
+        XP.putObject(getAccountingTablePath(),table);
+    }
 
 }
