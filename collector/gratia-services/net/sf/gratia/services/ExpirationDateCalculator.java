285f26531b7269f8f44a751d29ec5c93d67ebcf8
greenc <greenc@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2008-07-02 03:15:17 +0000

    Feature-complete.
    UNTESTED!
    
    Locking is probably conservative -- would probably benefit from a second
    opinion.
    
    
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@2243 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- collector/gratia-services/net/sf/gratia/services/ExpirationDateCalculator.java
+++ collector/gratia-services/net/sf/gratia/services/ExpirationDateCalculator.java
@@ -12,6 +12,9 @@ import java.util.TimeZone;
 import java.util.Date;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.Enumeration;
+import java.util.regex.*;
+import java.lang.Integer;
 
 public class ExpirationDateCalculator {
     // Caching handler class for calculating expiration dates of various
@@ -25,19 +28,136 @@ public class ExpirationDateCalculator {
     static Hashtable limitCache = new Hashtable(); // Pre-parsed limit values
     static Lock cacheLock = new ReentrantLock(); // Lock for concurrency issues
     static Properties p; // Everyone sees the same properties
+
+    Properties changeDetector;
+
     static SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
     static final Date invalidDate = new Date(0);
 
     Hashtable eDateCache = new Hashtable(); // Pre-calculated expiration dates still valid?
     String lastExpirationRefDate; // Pre-calculated expiration dates still valid?
 
-    public ExpirationDateCalculator() {
-        refreshLimits();
+    public enum DurationUnit {
+        DAY(Calendar.DAY_OF_MONTH),
+            WEEK(Calendar.WEEK_OF_YEAR),
+            MONTH(Calendar.MONTH),
+            YEAR(Calendar.YEAR),
+            UNLIMITED(Calendar.ERA);
+
+        private final int calField;
+
+        DurationUnit(int calField) { this.calField = calField; }
+
+        public int calField() { return calField; }
+    };
+
+    // Used by Duration class, below
+    static final DurationUnit defaultUnit = DurationUnit.MONTH;
+    static protected Pattern durationPattern =
+        Pattern.compile("(\\d+)\\s+([DWMY]|UNLIMITED)?",
+                        Pattern.CASE_INSENSITIVE);
+
+    public class DurationParseException extends Exception {
+        public DurationParseException(String message) {
+            super(message);
+        }
+        public DurationParseException(String message, Throwable cause) {
+            super(message, cause);
+        }
+        public DurationParseException(Throwable cause) {
+            super(cause);
+        }
     }
 
-    public Properties refreshLimits() {
-        p = net.sf.gratia.util.Configuration.getProperties();        
-        limitCache = new Hashtable();
+    class Duration {
+        int ordinality;
+        DurationUnit unit;
+
+        public int ordinality() {
+            return ordinality;
+        }
+
+        public DurationUnit unit() {
+            return unit;
+        }
+
+        public Duration(int ordinality, DurationUnit unit) {
+            this.ordinality = ordinality;
+            this.unit = unit;
+        }
+
+        public Duration(String property) throws DurationParseException {
+            Matcher m = durationPattern.matcher(property.toLowerCase());
+            if (m.lookingAt()) {
+                ordinality = Integer.parseInt(m.group(1));
+                char switchChar;
+                if (m.group(2) == null) {
+                    unit = defaultUnit;
+                } else {
+                    switch(m.group(2).charAt(0)) {
+                    case 'd':
+                        unit = DurationUnit.DAY;
+                        break;
+                    case 'w':
+                        unit = DurationUnit.WEEK;
+                        break;
+                    case 'm':
+                        unit = DurationUnit.MONTH;
+                        break;
+                    case 'y':
+                        unit = DurationUnit.YEAR;
+                        break;
+                    case 'u':
+                        unit = DurationUnit.UNLIMITED;
+                        break;
+                    default:
+                        throw new
+                            DurationParseException("unable to parse unit specification \"" +
+                                                   m.group(2) + "\"");
+                    }
+                }
+            } else {
+                throw new
+                    DurationParseException("ExpirationDateCalculator.Duration: unable to parse lifetime specification \"" +
+                                           property + "\"");
+            }
+        }
+    }
+
+    public ExpirationDateCalculator() {
+        cacheLock.lock();
+        try {
+            if (p == null) {
+                p = net.sf.gratia.util.Configuration.getProperties(); // Read once
+                Pattern lifetimePattern = Pattern.compile("service\\.lifetime\\.(.*)");
+                Enumeration properties = p.keys();
+                while (properties.hasMoreElements()) {
+                    String key = (String) properties.nextElement();
+                    Matcher m = lifetimePattern.matcher(key);
+                    if (m.lookingAt()) { // Match
+                        // Parse and cache
+                        try {
+                            limitCache.put(m.group(1), new Duration(p.getProperty(key)));
+                        }
+                        catch (DurationParseException e) {
+                            Logging.warning("ExpirationDateCalculator: found problem with lifetime property " +
+                                            key + ": error was, " +
+                                            e.getMessage() + " -- property ignored");
+                        }
+                    } else {
+                        // Trim properties object to only have lifetime properties.
+                        p.remove(key);
+                    }
+                }
+            }
+        }
+        finally {
+            cacheLock.unlock();
+        }
+        
+    }
+
+    public Properties lifetimeProperties() {
         return p;
     }
 
@@ -54,7 +174,67 @@ public class ExpirationDateCalculator {
 
     public Date expirationDate(Date refDate, String table,
                                String qualifier) {
-        return (Date) invalidDate.clone();
+        String key = table +
+            ((qualifier.length() > 0)?("." + qualifier):"");
+        String date = dateFormatter.format(refDate);
+        cacheLock.lock();
+        GregorianCalendar cal = new
+            GregorianCalendar(TimeZone.getTimeZone("GMT"));
+        try {
+            if (lastExpirationRefDate.equals(date)) {
+                if (eDateCache.containsKey(key)) {
+                    cacheLock.unlock();
+                    return (Date) eDateCache.get(key);
+                } else {
+                    // Calendar starts from the reference date
+                    try {
+                        cal.setTime(dateFormatter.parse(lastExpirationRefDate));
+                    }
+                    catch (java.text.ParseException e) {
+                        Logging.warning("ExpirationDateCalculator: internal error: could not parse " +
+                                        lastExpirationRefDate, e);
+                        cacheLock.unlock();
+                        return (Date) invalidDate.clone();
+                    }
+                }
+            } else {
+                // Reset cache.
+                eDateCache = new Hashtable();
+                // New calendar set to refDate
+                cal.setTime(refDate);
+                // Expiration date calculated with respect to the
+                // beginning of the day
+                cal.set(GregorianCalendar.MILLISECOND, 0);
+                cal.set(GregorianCalendar.SECOND, 0);
+                cal.set(GregorianCalendar.MINUTE, 0);
+                cal.set(GregorianCalendar.HOUR, 0);
+                lastExpirationRefDate = dateFormatter.format(cal.getTime());
+            }
+            // Obtain the duration from the limit cache
+            try {
+                Duration limitDuration =
+                    (Duration) limitCache.get(key);
+                if (limitDuration.unit().equals(DurationUnit.UNLIMITED)) {
+                    eDateCache.put(key, invalidDate);
+                    cacheLock.unlock();
+                    return (Date) invalidDate.clone();
+                } else {
+                    cal.add(limitDuration.unit().calField(),
+                            -1 * limitDuration.ordinality());
+                    eDateCache.put(key, cal.getTime());
+                    cacheLock.unlock();
+                    return cal.getTime();
+                }
+            }
+            catch (NullPointerException e) {
+                // No property for that, return invalid date;
+                cacheLock.unlock();
+                return (Date) invalidDate.clone();
+            }
+        }
+        finally {
+            cacheLock.unlock();
+        }
     }
 
     public Date expirationDate(Date refDate, String table) {
