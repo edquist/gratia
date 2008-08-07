168783430e13ca89243e9ba46de30d058651c561
jgweigand <jgweigand@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2008-08-07 18:47:12 +0000

    Modifications to allow for running this script from cron for the ITB nodes currently on gratia02
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@2389 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- build-scripts/gratia-upgrade.sh
+++ build-scripts/gratia-upgrade.sh
@@ -14,11 +14,21 @@ function ask_continue {
 }
 #------------------------------------
 function logit {
-  echo "$1"
+  logfile=$tomcat_dir/${tomcat}-upgrade.log
+  DATE="$(date '+%Y/%m/%d-%H:%M:%S')"
+  echo "$1" | tee -a $logfile
 }
 #------------------------------------
 function logerr {
-  logit;logit "ERROR: $1";logit;exit 1
+  logit;logit "ERROR- $1";logit
+  send_mail "FAILED" "$1"
+  log_upgrade_end 
+  exit 1
+}
+#------------------------------------
+function usage_error {
+  usage;echo;echo "ERROR- $1";echo
+  exit 1
 }
 #----------------------
 function try_again {
@@ -29,7 +39,7 @@ function try_again {
 }
 #------------------------------------
 function delimit {
-  logit;logit "----- $1 ----------"
+  logit "----- $1 ----------"
 }
 #-----------------------------------
 function usage {
@@ -45,59 +55,70 @@ There are 2 modes:
    Refer to the paragraph below as to the prompts for the value of the
    individual arguments.
      $PGM --instance TOMCAT_INSTANCE --source SOURCE_DIR --pswd ROOT_PSWD
+          [--daily  EMAIL_ADDRESS(ES) --mysql MYSQL_FILE]
 
 Although it does not really do much more than is already available to do, its
 intent is to take some of the guesswork (memory-like) out of the upgrade
 process.
 
-The only assumption it makes is that all tomcat collectors are in /data.
+A major assumption it makes is that all tomcat collectors are in $tomcat_dir.
 If this is not true, you will have to do it the old fashion way.
 
-The script will prompt you for:
- tomcat instance (e.g, tomcat-weigand)
- source directory
-   - daily builds..... /home/gratia/gratia-builds
-   - release builds... /home/gratia/gratia-releases
-   - other............ specified by you
- mysql root password
+ No prompt mode   Prompt mode questions
+ --------------   --------------------- 
+ --instance       tomcat instance (e.g, tomcat-weigand)
+ --source         source directory
+                    - daily builds..... /home/gratia/gratia-builds
+                    - release builds... /home/gratia/gratia-releases
+                    - other............ specified by you
+ --pswd           mysql root password
+ --daily          do you want to start the tomcat/collector service
+ --mysql          DB MySql file containing the password for upgrades 
+                  (required when running from cron and when used the 
+                  --pswd argument is ignored)
 
-The script then:
+If '--daily' is used, then all arguments are required and use of this argument
+will automatically start the tomcat/collector and requires specifying the
+email address(es).  It is intended for use ONLY when running from cron for 
+the purpose of daily test installations.
+
+The script performs the following:
  1. shutdown your tomcat instance/collector
  2. run update-gratia-local with the appropriate arguments
  3. clean your log directory saving the old logs in 
-      /data/gratia_tomcat_logs_backups
+      $tomcat_dir/gratia_tomcat_logs_backups
  4. optionally, allow you to start your collector
-    (when in 'no prompt' mode, it will start the collector)
+    (when the --daily argument is specified, it will start the collector)
 
 You must be root user to execute this script.
 "
 }
 #--------------------------------
 function initial_dialog {
-  logit "
+  echo "
 You can terminate this script at any time using <CNTL-C> EXCEPT after
 you give the FINAL approval to perform the update.
 
 This script will allow you to upgrade one of these Gratia collectors in 
 the $tomcat_dir directory on $tomcat_host:"
   cnt=0
-  for dir in $(ls -d $tomcat_dir/tomcat-*)
+  for dir in $(ls -d $tomcat_dir/tomcat-* |egrep -v "log|grep")
   do 
-    logit "$(ls -ld $dir)"
+    ls -ld $dir |egrep -v 'log|grep'
     if [ -w "$dir" ];then
       collectors="$collectors $(basename $dir)"
       cnt=1
     fi
   done
   if [ $cnt -eq 0 ];then
-    logerr "You lack permission to update any Gratia collector on this node.
-... bye"
+    echo "You lack permission to update any Gratia collector on this node.
+... bye":exit 1
   fi
   ask_continue
 }
 #--------------------------------
 function choose_collector {
-  delimit choose_collector
+  echo "----- choose_collector ------"
   while :
   do
     echo -n "
@@ -128,7 +149,7 @@ function validate_source {
 }
 #-------------------------------
 function choose_source_directory {
-  delimit choose_source_directory
+  echo "----- choose_source_directory ------"
   while :
   do
     echo -n "Official releases or nightly builds (releases/builds/other)? [default - $source_type]: "
@@ -146,7 +167,7 @@ function choose_source_directory {
 }
 #--------------------------------
 function find_release_source {
-  delimit find_release_source
+  echo "----- find_release_source ------"
   release_dir=/home/gratia/gratia-releases 
   while :
   do
@@ -163,7 +184,7 @@ $(ls -d $release_dir/gratia-v*)
 }
 #--------------------------------
 function find_build_source {
-  delimit find_build_source
+  echo "----- find_build_source ------"
   release_dir=/home/gratia/gratia-builds 
   while :
   do
@@ -180,7 +201,7 @@ $(ls -d $release_dir/gratia-* |egrep -v "\.log")
 }
 #--------------------------------
 function find_other_source {
-  delimit find_other_source
+  echo "----- find_other_source ------"
   while :
   do
     echo -n "Specify directory? [default - $release]: "
@@ -194,7 +215,7 @@ function find_other_source {
 }
 #--------------------------------
 function choose_db_root_password {
-  delimit  choose_db_root_password 
+  echo "----- choose_db_root_password ------"
   stty -echo   # turns off echo of keyboard
   read -p  "Enter the root MySql password [default - $pswd]: " ans
   stty echo    # turns echo back on
@@ -236,15 +257,19 @@ $(ls -l $backup_file)
 #------------------------------- 
 function install_upgrade {
   delimit install_upgrade
+  if [ "$mysql_file" != "NONE" ];then
+    pswd="$(cat $mysql_file)"
+  fi
   cmd="$pgm -d $pswd -S $source -s $(echo $tomcat|cut -d'-' -f2)"
 echo "
 ... executing:
 $cmd"
   sleep 3
-  ${cmd};rtn=$?
-  if [ "$rtn" != "0" ];then
-    logerr "Install FAILED!!!!"
-  fi
+  (${cmd};rtn=$?
+   if [ "$rtn" != "0" ];then
+     logerr "Install FAILED!!!!"
+   fi
+  ) 2>&1 |tee -a $logfile
   logit "Install was successful"
   sleep 3
 }
@@ -265,7 +290,7 @@ function verify_the_update_program_exists {
 #-------------------------------
 function finish_up {
   delimit finish_up
-  echo "
+  logit "
 You have installed the following release of the Gratia collector:
 $(cat $tomcat_dir/$tomcat/gratia/gratia-release)
 
@@ -276,14 +301,14 @@ You can then check the log files
       cd $tomcat_dir/$tomcat/logs
       tail -f *
 
-Cron scripts that should be tested/executed:
+Cron scripts that should be tested/executed after you start the collector:
 $(crontab -l | grep $tomcat_dir/$tomcat)
 
 "
 }
 #-------------------------------
 function ask_to_start_collector {
-  delimit ask_to_start_collector
+  echo "----- ask_to_start_collector -----"
   echo -n "Do you want to start the collector: (y/n): "
   read ans
   if [ "$ans" = "y" ];then
@@ -292,15 +317,54 @@ function ask_to_start_collector {
 }
 #-----------------------
 function start_collector {
+  delimit start_collector
   cmd="service $(echo $tomcat |cut -d'/' -f3) start"
   logit "... starting collector"
   logit "    $cmd" 
-  $cmd
+  ( $cmd;rtn=$?
+    if [ "$rtn" != "0" ];then
+      logerr "Non-zero return code ($rtn) on collector startup"
+    fi
+  ) 2>&1 | tee -a $logfile
+}
+#-----------------------
+function ask_to_run_static_reports {
+  echo "-----  ask_to_run_static_reports -----"
+  echo -n "Do you want to run the static reports: (y/n): "
+  read ans
+  if [ "$ans" = "y" ];then
+    run_static_reports
+  fi
+}
+#-----------------------
+function run_static_reports {
+  delimit run_static_reports
+  pdf_dir=$tomcat_dir/$tomcat/webapps/reports-static
+  csv_dir=$tomcat_dir/$tomcat/webapps/reports-static_csv
+  case $tomcat in 
+   "itb_gratia_itb" ) logit "... sleeping 20 seconds to allow tomcat to initialize"
+                      sleep 20 
+                      cmd="$(crontab -l | grep $tomcat_dir/$tomcat |awk '{print $6,$7,$8}')"
+                      logit "RUNNING: $cmd"
+                      ( $cmd;rtn=$?
+                        if [ "$rtn" != "0" ];then
+                          logerr "Non-zero return code ($rtn) on static reports"
+                        fi
+                      ) 2>&1 | tee -a $logfile
+                      if [ ! -d "$pdf_dir" ];then
+                        logerr "Static reports not available. Directory does not exist: $pdf_dir"
+                      fi
+                      if [ ! -d "$csv_dir" ];then
+                        logerr "Static reports not available. Directory does not exist: $csv_dir"
+                      fi
+                     ;;
+   * ) logit "... static reports not used for $tomcat";;
+  esac     
 }
 #--------------------------------
 function final_verification {
-  delimit final_verification
-  logit "
+  echo "----- final_verification ------"
+echo "
 We are ready to upgrade with the following information provided:
   host............... $(hostname -f)
   tomcat instance.... $tomcat
@@ -310,44 +374,138 @@ We are ready to upgrade with the following information provided:
   ask_continue
 }
 #-----------------------------------
+function log_final_verification {
+  logit "\
+Upgrading with the following information provided:
+  host............... $(hostname -f)
+  tomcat instance.... $tomcat
+  source location.... $source
+  password........... cannot say
+"
+  sleep 2
+}
+#-----------------------------------
 function verify_root_user {
   if [ "$(id -u)" != "0" ];then
     logerr "You must be root user!"
   fi
 }
+#-------------------------------------
+function send_mail {
+  status="$1"
+  msg="$2"
+  if [ -z "$recipients" ];then
+    recipients=$default_recipients
+  fi
+  logit "... sending mail to $recipients"
+  subject="Gratia upgrade: $(hostname -s) of $tomcat - $status"
+
+##cat <<EOF
+##mail -s "$subject" $recipients
+mail -s "$subject" $recipients <<EOF
+$subject
+
+This is an automated message from $PGM running on $(hostname -s). 
+
+Check the following logs for possible causes:
+  $logfile
+  $tomcat_dir/$tomcat/logs files.
+
+$msg
+EOF
+}
+#----------------------------
+function log_upgrade_start {
+  logit
+  logit "=============================================================="
+  logit "Upgrade of $tomcat on $(hostname -s) started: $(date)"
+  logit "Program: $PGM"
+  logit "Log file: $logfile"
+  logit
+}
+#----------------------------
+function log_upgrade_end {
+  logit
+  logit "Log file: $logfile"
+  logit "Upgrade of $tomcat on $(hostname -s) completed: $(date)"
+  logit "=============================================================="
+  logit
+}
+#----------------------------
+function process_in_prompt_mode {
+  choose_collector
+  choose_source_directory
+  choose_db_root_password 
+  final_verification
+  log_upgrade_start
+  log_final_verification
+  verify_the_makefile_target_dir_exists 
+  verify_the_update_program_exists
+  install_upgrade
+  clean_log_directory 
+  finish_up
+  ask_to_start_collector
+  ask_to_run_static_reports
+  log_upgrade_end
+}
+#----------------------------
+function process_in_no_prompt_mode {
+  validate_collector
+  verify_the_makefile_target_dir_exists 
+  verify_the_update_program_exists
+  log_upgrade_start
+  log_final_verification
+  install_upgrade
+  clean_log_directory 
+  finish_up
+  if [ "$daily" = "yes" ];then
+    start_collector 
+    run_static_reports
+  else
+    ask_to_start_collector
+  fi
+  log_upgrade_end
+}
 #### MAIN ##############################################
 PGM=$(basename $0)
 tomcat_tarball=/home/gratia/tomcat-tarballs/apache-tomcat-5.5.25.tar.gz
+
+daily=NONE
 new_instance="no"
-do_more_than_once="yes"
 prompt=yes
+pswd=NONE
+qa_mode=yes
 source_type=""
 source=NONE
-pswd=NONE
+##default_recipients="weigand@fnal.gov greenc@fnal.gov pcanal@fnal.gov fermigrid-operations@fnal.gov"
+default_recipients="weigand@fnal.gov"
+recipients=""
 release=NONE
+release_dir=""
 tomcat=NONE
 tomcat_host=$(hostname -s)
 tomcat_dir=/data
 log_backup_dir=$tomcat_dir/gratia_tomcat_logs_backups
-release_dir=""
 update_pgm=common/configuration/update-gratia-local
+mysql_file=NONE
+
 
 #--- get command line arguements ----
 while test "x$1" != "x"; do
-   if [ "$1" == "--help" ]; then
+   if [ "$1" == "--help" ];then
         HELP="yes";shift
-#   elif [ "$1" == "--new" ]; then
-#        new_instance="yes"
-#        shift
-   elif [ "$1" == "--instance" ]; then
-        tomcat=$2;shift 2
-   elif [ "$1" == "--source" ]; then 
-        source="$2";shift 2
-   elif [ "$1" == "--pswd" ]; then 
-        pswd="$2";shift 2
+   elif [ "$1" == "--instance" ];then
+        tomcat="$2";shift;shift
+   elif [ "$1" == "--source" ];then 
+        source="$2";shift;shift
+   elif [ "$1" == "--pswd" ];then 
+        pswd="$2";shift;shift
+   elif [ "$1" == "--daily" ];then
+        daily="yes";recipients="$2";shift;shift
+   elif [ "$1" == "--mysql" ];then
+        mysql_file="$2";shift;shift
    else
-        usage
-        logerr "Invalid command line argument: $1"
+        usage_error "Invalid command line argument: $1"
    fi
 done
 
@@ -361,51 +519,54 @@ verify_root_user
 if [ "$tomcat" != "NONE" ] && \
    [ "$source" != "NONE" ] && \
    [ "$pswd"   != "NONE" ];then
-  do_more_than_one=no
   prompt=no
 elif [ "$tomcat" = "NONE" ] && \
      [ "$source" = "NONE" ] && \
-     [ "$pswd"   = "NONE" ];then
-  do_more_than_one=yes
+     [ "$pswd"   = "NONE" ] && \
+     [ "$daily"  = "NONE" ] && \
+     [ "$mysql_file"  = "NONE" ];then
   prompt=yes
   initial_dialog
 else
-  usage
-  logerr "Missing one or more required arguments."
+  usage_error "Missing one or more required arguments."
+fi
+
+#-- verify some required args -----
+if [ "$daily" = "yes" ];then
+  if [ -z "$recipients" ];then
+    usage_error "Email address is required when --daily is used"
+  fi
+  if [ -z "$mysql_file" ];then
+    usage_error "--mysql argument required when --daily is used"
+  fi
+  if [ ! -e "$mysql_file" ];then
+    usage_error "--mysql file ($mysql_file) does not exist."
+  fi
+fi
+
+#--- this is so we don't write badly named log files in $tomcat_dir --
+if [ "$prompt" = "no" ];then
+  if [ ! -d "$tomcat_dir/$tomcat" ];then
+    usage_error "--instance directory ($tomcat_dir/$tomcat) does not exist."
+  fi
 fi
 
 #--- do it -----------
+qa_mode=yes
 while 
- [ "$do_more_than_once" = "yes" ]
+ [ "$qa_mode" = "yes" ]
 do 
   case $prompt in 
-    "yes" ) do_more_than_once="yes"
-            choose_collector
-            choose_source_directory
-            choose_db_root_password 
-            final_verification
-            verify_the_makefile_target_dir_exists 
-            verify_the_update_program_exists
-            install_upgrade
-            clean_log_directory 
-            finish_up
-            ask_to_start_collector
+    "yes" ) qa_mode="yes"  # allows multiple upgrades to be performed
+            process_in_prompt_mode
             ;;
-
-   "no"  ) do_more_than_once="no"
-           validate_collector
-           verify_the_makefile_target_dir_exists 
-           verify_the_update_program_exists
-           install_upgrade
-           clean_log_directory 
-           finish_up
-           start_collector 
+   "no"  ) qa_mode="no"   # forces a single upgrade to be performed
+           process_in_no_prompt_mode
            ;;
-    * ) logerr "System error - problem in this script. This should never occur.";;
+    * ) usage_error "System error - problem in this script. This should never occur.";;
   esac
 done
 
-logit  DONE
 exit 0
 
 
