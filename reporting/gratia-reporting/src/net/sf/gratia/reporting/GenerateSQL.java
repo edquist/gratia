179123b2f9c8ec57c8395b085a506eaa777963f5
penelopec <penelopec@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2008-10-09 15:39:34 +0000

    Eliminated unused code and fixed field names for new trace table.
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@2642 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- reporting/gratia-reporting/src/net/sf/gratia/reporting/GenerateSQL.java
+++ reporting/gratia-reporting/src/net/sf/gratia/reporting/GenerateSQL.java
@@ -6,161 +6,92 @@ import java.sql.*;
 
 public class GenerateSQL
 {
-      XP xp = new XP();
-      //
-      // database related
-      //
-      String driver = "";
-      String url = "";
-      String user = "";
-      String password = "";
-      Connection connection;
-      Statement statement;
-      ResultSet resultSet;
-      //
-      // processing related
-      //
-      String key = null;
-      String role = null;
-      String policy = "Unknown";
-      String sql = null;
-      String p1 = null;
-      String p2 = null;
-      String p3 = null;
-      String p4 = null;
-      String p5 = null;
-      String p6 = null;
-      String p7 = null;
-      String p8 = null;
-      String p9 = null;
-      String data = null;
-      
-      String dq = "'";
-    
-      public GenerateSQL(String sql,String key)
+	//
+	// database related
+	//
+	String driver = "";
+	String url = "";
+	String user = "";
+	String password = "";
+	Connection connection;
+	Statement statement;
+	ResultSet resultSet;
+
+	//
+	// processing related
+	//
+	String dq = "'";
+	String data = null;
+	String sql = null;
+	String key = null;
+
+	public GenerateSQL(String sql, String key)
       {
             this.sql = sql;
             this.key = key;
       }
 
-      public String generate()
-      {
-            openConnection();
+	public String generate()
+	{
+		data = "";
+		openConnection();
 
-            String command = "select * from trace where userkey = " + dq + key + dq;
-	    //System.err.println("command: "+command);
-            try
-                  {
-                        statement = connection.prepareStatement(command);
-                        resultSet = statement.executeQuery(command);
-                        while(resultSet.next())
-                              {
-                                    data = resultSet.getString("data");
-                                    p1 = resultSet.getString("p1");
-                                    p2 = resultSet.getString("p2");
-                                    p3 = resultSet.getString("p3");
-                                    p4 = resultSet.getString("p4");
-                                    p5 = resultSet.getString("p5");
-                                    p6 = resultSet.getString("p6");
-                                    p7 = resultSet.getString("p7");
-                                    p8 = resultSet.getString("p8");
-                                    p9 = resultSet.getString("p9");
-                                    role = resultSet.getString("role");
-                              }
-                        resultSet.close();
-                        statement.close();
-                  }
-            catch (Exception e)
-                  {
-                        e.printStackTrace();
-                        closeConnection();
-                        return sql;
-                  }
-            command = "select whereclause from Role where role = " + dq + role + dq;
-	    //System.err.println("command: "+command);
-            try
-                  {
-                        statement = connection.prepareStatement(command);
-                        resultSet = statement.executeQuery(command);
-                        while(resultSet.next())
-                              {
-                                    policy = resultSet.getString("whereclause");
-                              }
-                        resultSet.close();
-                        statement.close();
-                  }
-            catch (Exception e)
-                  {
-                        e.printStackTrace();
-                        closeConnection();
-                        return sql;
-                  }
-            closeConnection();
-            //
-            // only regenerate for role GratiaGlobalAdmin or policy Everything
-            //
-          //  if ((! role.equals("GratiaGlobalAdmin")) && (! policy.equals("Everything")))
-            //      return sql;
+		String command = "select * from trace where userKey = " + dq + key + dq;
+		//System.err.println("command: "+command);
+		try
+		{
+			statement = connection.prepareStatement(command);
+			resultSet = statement.executeQuery(command);
+			while(resultSet.next())
+			{
+				data = resultSet.getString("sqlQuery");
+			}
+			resultSet.close();
+			statement.close();
+			closeConnection();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			closeConnection();
+			sql = "";
+		}
+		return data;
+	}
 
-	    //System.err.println("Pass policy check");
+	public void openConnection()
+	{
+		try
+		{
+			Properties p = Configuration.getProperties();
+			driver = p.getProperty("service.mysql.driver");
+			url = p.getProperty("service.mysql.url");
+			user = p.getProperty("service.reporting.user");
+			password = p.getProperty("service.reporting.password");
+		}
+		catch (Exception ignore)
+		{
+		}
+		try
+		{
+			Class.forName(driver).newInstance();
+			connection = DriverManager.getConnection(url,user,password);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 
-            //
-            // otherwise walk the parameter list and return the reconstituted sql
-            //
-            if (data.indexOf("?") > 0)
-                  data = xp.replace(data,"?",dq + p1 + dq);
-            if (data.indexOf("?") > 0)
-                  data = xp.replace(data,"?",dq + p2 + dq);
-            if (data.indexOf("?") > 0)
-                  data = xp.replace(data,"?",dq + p3 + dq);
-            if (data.indexOf("?") > 0)
-                  data = xp.replace(data,"?",dq + p4 + dq);
-            if (data.indexOf("?") > 0)
-                  data = xp.replace(data,"?",dq + p5 + dq);
-            if (data.indexOf("?") > 0)
-                  data = xp.replace(data,"?",dq + p6 + dq);
-            if (data.indexOf("?") > 0)
-                  data = xp.replace(data,"?",dq + p7 + dq);
-            if (data.indexOf("?") > 0)
-                  data = xp.replace(data,"?",dq + p8 + dq);
-            if (data.indexOf("?") > 0)
-                  data = xp.replace(data,"?",dq + p9 + dq);
-            return data;
-      }
-
-      public void openConnection()
-      {
-            try
-                  {
-                        Properties p = Configuration.getProperties();
-                        driver = p.getProperty("service.mysql.driver");
-                        url = p.getProperty("service.mysql.url");
-                        user = p.getProperty("service.reporting.user");
-                        password = p.getProperty("service.reporting.password");
-                  }
-            catch (Exception ignore)
-                  {
-                  }
-            try
-                  {
-                        Class.forName(driver).newInstance();
-                        connection = DriverManager.getConnection(url,user,password);
-                  }
-            catch (Exception e)
-                  {
-                        e.printStackTrace();
-                  }
-      }
-
-      public void closeConnection()
-      {
-            try
-                  {
-                        connection.close();
-                  }
-            catch (Exception e)
-                  {
-                        e.printStackTrace();
-                  }
-      }
+	public void closeConnection()
+	{
+		try
+		{
+			connection.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
