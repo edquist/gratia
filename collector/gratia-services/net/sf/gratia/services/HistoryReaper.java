fa9fb43ad4fc4e075757fa681e34315c1b231f3c
greenc <greenc@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2009-02-02 23:09:03 +0000

    HistoryReaper is now:
    1. Runnable;
    
    2. Careful;
    
    3. Shorn of unused DB connection guff.
    
    
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@2948 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- collector/gratia-services/net/sf/gratia/services/HistoryReaper.java
+++ collector/gratia-services/net/sf/gratia/services/HistoryReaper.java
@@ -7,6 +7,7 @@ import net.sf.gratia.util.Configuration;
 import net.sf.gratia.util.Logging;
 
 import java.util.*;
+import java.util.concurrent.locks.*;
 import java.sql.*;
 import java.io.*;
 import java.text.*;
@@ -14,76 +15,80 @@ import java.text.*;
 import org.apache.tools.bzip2.*;
 import com.ice.tar.*;
 
-public class HistoryReaper
-{
-   public String driver;
-   public String url;
-   public String user;
-   public String password;
 
-   Connection connection;
-   Statement statement;
-   ResultSet resultSet;
+public class HistoryReaper implements Runnable {
+    Properties p;
 
-   String command;
+    String filenames[] = new String[0];
 
-   Properties p;
+    private static Lock reaperLock = new ReentrantLock();
+    private static ReadWriteLock statusLock = new ReentrantReadWriteLock();
 
-   XP xp = new XP();
+    private static Boolean in_progress = false;
 
-   String filenames[] = new String[0];
+    public static Boolean inProgress() {
+        try {
+            statusLock.readLock().lock();
+            return in_progress;
+        }
+        finally {
+            statusLock.readLock().unlock();
+        }
+    }
 
-   public HistoryReaper()
-   {
-      Logging.fine("HistoryReaper: Starting");
+    private static void setRunStatus(Boolean status) {
+        statusLock.writeLock().lock();
+        try {
+            in_progress = status;
+        }
+        finally {
+            statusLock.writeLock().unlock();
+        }
+    }
 
+    public HistoryReaper() {
       p = net.sf.gratia.util.Configuration.getProperties();
-      driver = p.getProperty("service.mysql.driver");
-      url = p.getProperty("service.mysql.url");
-      user = p.getProperty("service.mysql.user");
-      password = p.getProperty("service.mysql.password");
-      openConnection();
-      getDirectories();
-   }
+    }
 
-   public void openConnection()
-   {
-      try
-      {
-         Class.forName(driver).newInstance();
-         connection = null;
-         connection = DriverManager.getConnection(url, user, password);
-      }
-      catch (Exception e)
-      {
-      }
-   }
+    public void run() {
+        reaperLock.lock();
+        try {
+            setRunStatus(true);
+            Logging.fine("HistoryReaper: Starting.");
+            getDirectories();
+            Logging.fine("HistoryReaper: Complete.");
+        }
+        finally {
+            setRunStatus(false);
+            reaperLock.unlock();
+        }
+    }
 
-   public void deleteDirectory(String path)
-   {
-      String files[] = xp.getFileList(path);
+    public void deleteDirectory(String path)
+    {
+        String files[] = XP.getFileList(path);
 
-      for (int i = 0; i < files.length; i++)
-      {
-         try
-         {
-            File file = new File(files[i]);
-            file.delete();
-         }
-         catch (Exception ignore)
-         {
-         }
-      }
-      try
-      {
-         File file = new File(path);
-         file.delete();
-         Logging.log("HistoryReaper: Deleted Directory: " + path + ":Files: " + files.length);
-      }
-      catch (Exception ignore)
-      {
-      }
-   }
+        for (int i = 0; i < files.length; i++)
+            {
+                try
+                    {
+                        File file = new File(files[i]);
+                        file.delete();
+                    }
+                catch (Exception ignore)
+                    {
+                    }
+            }
+        try
+            {
+                File file = new File(path);
+                file.delete();
+                Logging.log("HistoryReaper: Deleted Directory: " + path + ":Files: " + files.length);
+            }
+        catch (Exception ignore)
+            {
+            }
+    }
 
     private static void addFiles(File file, TarOutputStream tos,String strip) throws IOException, FileNotFoundException
     {
@@ -133,6 +138,7 @@ public class HistoryReaper
         File directory = new File(dir);
         File filelist[] = directory.listFiles();
 
+        int compress_ops = 0;
         for (int i = 0; i < filelist.length; i++)
             {
                 File file = filelist[i];
@@ -150,115 +156,118 @@ public class HistoryReaper
                         //tos.setLongFileMode(TarOutputStream.LONGFILE_GNU);
                         addFiles(file,tos,dir);
                         tos.close();
- 
+                        ++compress_ops;
+                        if ((compress_ops % 5) == 0) {
+                            GCHelper.runGC(); // Trigger manual GC because we
+                            // don't seem to be doing it often
+                            // enough in this tight loop otherwise.
+                        }
                     } catch (Exception e) {
-                        
                         Logging.warning("HistoryReaper: Compression or deletion of "+file.getAbsolutePath()+" failed\nError: "+e.getMessage());
                     }
-                        
                 }
             }
     }
+    
+    public void getDirectories()
+    {
+        int i = 0;
+        Vector history = new Vector();
+        Vector old = new Vector();
+        String path = System.getProperties().getProperty("catalina.home") + "/gratia/data";
+        path = XP.replaceAll(path, "\\", "/");
+        Logging.log("HistoryReaper: Path: " + path);
+        String temp[] = XP.getDirectoryList(path);
+        for (i = 0; i < temp.length; i++)
+            {
+                String directory = temp[i];
+                if (directory.indexOf("history") > -1) 
+                    {
+                        history.add(temp[i]);
+                    }
+                else if (directory.indexOf("old") > -1)
+                    {
+                        old.add(temp[i]);
+                    }
+            }
+        Logging.log("HistoryReaper: 'old' Directories To Process: " + history.size());
+        Logging.log("HistoryReaper: 'history' Directories To Process: " + old.size());
+        //
+        // figure out which directories to delete
+        //
+        java.util.Date now = new java.util.Date();
+        long nowmilli = now.getTime();
 
-   public void getDirectories()
-   {
-      int i = 0;
-      Vector history = new Vector();
-      Vector old = new Vector();
-      String path = System.getProperties().getProperty("catalina.home") + "/gratia/data";
-      path = xp.replaceAll(path, "\\", "/");
-      Logging.log("HistoryReaper: Path: " + path);
-      String temp[] = xp.getDirectoryList(path);
-      for (i = 0; i < temp.length; i++)
-      {
-         String directory = temp[i];
-         if (directory.indexOf("history") > -1) 
-         {
-            history.add(temp[i]);
-         }
-         else if (directory.indexOf("old") > -1)
-         {
-            old.add(temp[i]);
-         }
-      }
-      Logging.log("HistoryReaper: 'old' Directories To Process: " + history.size());
-      Logging.log("HistoryReaper: 'history' Directories To Process: " + old.size());
-      //
-      // figure out which directories to delete
-      //
-      java.util.Date now = new java.util.Date();
-      long nowmilli = now.getTime();
+        long historymilli = Long.parseLong(p.getProperty("maintain.history.log"));
+        Logging.log("HistoryReaper: using default compress time ("+historymilli+" days)");
+        historymilli = historymilli * 24 * 60 * 60 * 1000;
+        java.util.Date oldest = new java.util.Date(nowmilli - historymilli);
+        Calendar beginning = Calendar.getInstance();
+        beginning.setTime(oldest);
+        beginning.set(Calendar.SECOND, 0);
+        beginning.set(Calendar.MINUTE, 0);
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddhh");
 
-      long historymilli = Long.parseLong(p.getProperty("maintain.history.log"));
-      Logging.log("HistoryReaper: using default compress time ("+historymilli+" days)");
-      historymilli = historymilli * 24 * 60 * 60 * 1000;
-      java.util.Date oldest = new java.util.Date(nowmilli - historymilli);
-      Calendar beginning = Calendar.getInstance();
-      beginning.setTime(oldest);
-      beginning.set(Calendar.SECOND, 0);
-      beginning.set(Calendar.MINUTE, 0);
-      SimpleDateFormat format = new SimpleDateFormat("yyyyMMddhh");
+        long compressmilli = 2;
+        try {
+            compressmilli = Long.parseLong(p.getProperty("maintain.history.compress"));
+            Logging.log("HistoryReaper: using configure compress time ("+compressmilli+" hours)");
+        } catch (Exception e) {
+            Logging.log("HistoryReaper: using default compress time ("+compressmilli+" hours)");
+        }
+        compressmilli = compressmilli * 60 * 60 * 1000;
+        java.util.Date coldest = new java.util.Date(nowmilli - compressmilli);
+        Calendar cbeginning = Calendar.getInstance();
+        cbeginning.setTime(coldest);
+        cbeginning.set(Calendar.SECOND, 0);
+        cbeginning.set(Calendar.MINUTE, 0);
+        SimpleDateFormat cformat = new SimpleDateFormat("yyyyMMddhh");
 
-      long compressmilli = 2;
-      try {
-          compressmilli = Long.parseLong(p.getProperty("maintain.history.compress"));
-          Logging.log("HistoryReaper: using configure compress time ("+compressmilli+" hours)");
-      } catch (Exception e) {
-          Logging.log("HistoryReaper: using default compress time ("+compressmilli+" hours)");
-      }
-      compressmilli = compressmilli * 60 * 60 * 1000;
-      java.util.Date coldest = new java.util.Date(nowmilli - compressmilli);
-      Calendar cbeginning = Calendar.getInstance();
-      cbeginning.setTime(coldest);
-      cbeginning.set(Calendar.SECOND, 0);
-      cbeginning.set(Calendar.MINUTE, 0);
-      SimpleDateFormat cformat = new SimpleDateFormat("yyyyMMddhh");
+        String beginningHistory = path + "/history-" + format.format(beginning.getTime());
+        Logging.log("HistoryReaper: First History Directory To Process: " + beginningHistory);
 
-      String beginningHistory = path + "/history-" + format.format(beginning.getTime());
-      Logging.log("HistoryReaper: First History Directory To Process: " + beginningHistory);
+        String beginningOld = path + "/old-" + format.format(beginning.getTime());
+        Logging.log("HistoryReaper: First Old Directory To Process: " + beginningOld);
 
-      String beginningOld = path + "/old-" + format.format(beginning.getTime());
-      Logging.log("HistoryReaper: First Old Directory To Process: " + beginningOld);
+        String cbeginningHistory = path + "/history-" + cformat.format(cbeginning.getTime());
+        Logging.log("HistoryReaper: First History Directory To Compress: " + cbeginningHistory);
 
-      String cbeginningHistory = path + "/history-" + cformat.format(cbeginning.getTime());
-      Logging.log("HistoryReaper: First History Directory To Compress: " + cbeginningHistory);
+        String cbeginningOld = path + "/old-" + cformat.format(cbeginning.getTime());
+        Logging.log("HistoryReaper: First Old Directory To Compress: " + cbeginningOld);
 
-      String cbeginningOld = path + "/old-" + cformat.format(cbeginning.getTime());
-      Logging.log("HistoryReaper: First Old Directory To Compress: " + cbeginningOld);
+        //
+        // now - screen/delete older directories
+        //
+        String oldhistory = path + "/history2";
+        String oldold = path + "/old2";
+        for (i = 0; i < history.size(); i++)
+            {
+                String directory = (String)history.elementAt(i);
+                if (directory.startsWith(oldhistory) || (directory.compareTo(beginningHistory) < 0))
+                    {
+                        Logging.fine("HistoryReaper: Deleting Directory: " + directory);
+                        deleteDirectory(directory);
+                    }
+                else if (directory.startsWith(oldold) || directory.compareTo(cbeginningHistory) < 0)
+                    {
+                        Logging.fine("HistoryReaper: Compressing Directory: " + directory);
+                        compressDirectory(directory);
+                    }
+            }
+        for (i = 0; i < old.size(); i++)
+            {
+                String directory = (String)old.elementAt(i);
+                if (directory.compareTo(beginningOld) < 0)
+                    {
+                        Logging.fine("HistoryReaper: Deleting Directory: " + directory);
+                        deleteDirectory(directory);
 
-      //
-      // now - screen/delete older directories
-      //
-      String oldhistory = path + "/history2";
-      String oldold = path + "/old2";
-      for (i = 0; i < history.size(); i++)
-      {
-         String directory = (String)history.elementAt(i);
-         if (directory.startsWith(oldhistory) || (directory.compareTo(beginningHistory) < 0))
-         {
-            Logging.fine("HistoryReaper: Deleting Directory: " + directory);
-            deleteDirectory(directory);
-         }
-         else if (directory.startsWith(oldold) || directory.compareTo(cbeginningHistory) < 0)
-         {
-            Logging.fine("HistoryReaper: Compressing Directory: " + directory);
-            compressDirectory(directory);
-         }
-      }
-      for (i = 0; i < old.size(); i++)
-      {
-         String directory = (String)old.elementAt(i);
-         if (directory.compareTo(beginningOld) < 0)
-         {
-            Logging.fine("HistoryReaper: Deleting Directory: " + directory);
-            deleteDirectory(directory);
-
-         } else if (directory.compareTo(cbeginningOld) < 0)
-         {
-            Logging.fine("HistoryReaper: Compressing Directory: " + directory);
-            compressDirectory(directory);
-         } 
-      }
-   }
+                    } else if (directory.compareTo(cbeginningOld) < 0)
+                    {
+                        Logging.fine("HistoryReaper: Compressing Directory: " + directory);
+                        compressDirectory(directory);
+                    } 
+            }
+    }
 
 }
