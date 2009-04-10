43ce23050709b63a021ad057acca4c0da4358254
pcanal <pcanal@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2009-04-10 14:41:18 +0000

    use python friendly setting of http_proxy
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@3105 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- reporting/summary/daily_mail_cron.sh
+++ reporting/summary/daily_mail_cron.sh
@@ -4,7 +4,7 @@
 . /usr/local/etc/setups.sh 
 setup mysql > /dev/null 2>&1
 
-export http_proxy=squid.fnal.gov:3128 
+export http_proxy=http://squid.fnal.gov:3128 
 where=`dirname $0`
 cd $where
 $where/daily_mutt.sh $1
--- reporting/summary/range_mail_cron.sh
+++ reporting/summary/range_mail_cron.sh
@@ -3,7 +3,7 @@
 . /usr/local/etc/setups.sh 
 setup mysql
 
-export http_proxy=squid.fnal.gov:3128
+export http_proxy=http://squid.fnal.gov:3128
 where=`dirname $0`
 cd $where
 $where/range_mutt.sh $1 $2
