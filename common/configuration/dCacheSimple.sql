6d7a1261fea369ac666e1ee077f5e05cb0d684e1
greenc <greenc@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2009-07-10 15:51:28 +0000

    dCacheSimple is now a shell around TransferSimple for backward compatibility.
    TransferSimple is a (much) more efficient query than the old dCacheSimple and takes arguments for StartMonth and EndMonth (either or both may be NULL, but they must be specified in the call).
    
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@3271 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- common/configuration/dCacheSimple.sql
+++ common/configuration/dCacheSimple.sql
@@ -1,36 +1,59 @@
-DELIMITER $$
+DELIMITER ||
 
-DROP PROCEDURE IF EXISTS `dCacheSimple`$$
+-- Drop and recreate lookup table for multipliers to avoid awkward joins.
+DROP TABLE IF EXISTS SizeUnits
+||
+CREATE TABLE SizeUnits
+(Unit VARCHAR(8) NOT NULL PRIMARY KEY, Multiplier DOUBLE NOT NULL DEFAULT 0.0)
+||
+-- Populate it. Base unit is MiB
+INSERT INTO SizeUnits VALUES
+('b', 1 / 1024 / 1024),
+('kb', 1 / 1024),
+('mb', 1),
+('gb', 1024),
+('tb', 1024 * 1024),
+('pb', 1024 * 1024 * 1024),
+('eb', 1024 * 1024 * 1024 * 1024)
+ ||
+-- Drop and create backward-compatible procedure
+DROP PROCEDURE IF EXISTS `dCacheSimple`
+||
 CREATE PROCEDURE `dCacheSimple`()
+SQL SECURITY INVOKER
 READS SQL DATA
-begin
-select dMonth as `Date`, ProbeName, SiteName, sum(X.Njobs) as Njobs,
-       sum(X.`MB transferred`) as `MB Transferred`
- from ((select date_format(StartTime, '%Y-%m') as dMonth,
-               S.SiteName, T.ProbeName,
-               T.Njobs as Njobs,
-               T.TransferSize /1024/1024 as `MB transferred`
-from MasterTransferSummary T
-     join Probe P on (T.ProbeName = P.probename)
-     join Site S on (P.siteid = S.siteid)
-where T.StorageUnit = 'b') UNION ALL
-(select date_format(StartTime, '%Y-%m') as dMonth,
-               S.SiteName, T.ProbeName,
-       T.Njobs as Njobs,
-       T.TransferSize /1024 as `MB transferred`
-from MasterTransferSummary T
-     join Probe P on (T.ProbeName = P.probename)
-     join Site S on (P.siteid = S.siteid)
-where T.StorageUnit = 'kb') UNION ALL
-(select date_format(StartTime, '%Y-%m') as dMonth,
-               S.SiteName, T.ProbeName,
-       T.Njobs as Njobs,
-       T.TransferSize as `MB transferred`
-from MasterTransferSummary T
-     join Probe P on (T.ProbeName = P.probename)
-     join Site S on (P.siteid = S.siteid)
-where T.StorageUnit = 'mb')) X
-group by dMonth, ProbeName, SiteName
-order by dMonth desc, ProbeName, SiteName;
-END $$
+BEGIN
+CALL TransferSimple(NULL, NULL);
+END
+||
+-- Drop and create query procedure
+DROP PROCEDURE IF EXISTS `TransferSimple`
+||
+CREATE PROCEDURE `TransferSimple`(StartMonth DATE,
+  EndMonth DATE)
+SQL SECURITY INVOKER
+READS SQL DATA
+BEGIN
+SELECT 
+DATE_FORMAT(StartTime, '%Y-%m') AS `Date`,
+S.SiteName as `Site Name`,
+T.ProbeName as `Probe Name`,
+SUM(T.Njobs) AS `# Jobs`,
+SUM(T.TransferSize * Multiplier) AS `Data Transferred (MiB)`
+FROM
+MasterTransferSummary T
+JOIN Probe P ON (T.ProbeName = P.probename)
+JOIN Site S ON (P.siteid = S.siteid)
+JOIN SizeUnits SU ON (T.StorageUnit = SU.Unit)
+WHERE StartTime >= DATE_FORMAT(IFNULL(StartMonth, '2006-01-01'), '%Y-%m-01')
+  AND StartTime < DATE_FORMAT(IFNULL(EndMonth, NOW()) + INTERVAL 1 MONTH, '%Y-%m-01')
+GROUP BY `Date`, `Probe Name`, `Site Name`
+ORDER BY `Date` desc, `Probe Name`, `Site Name`;
+END
+||
 DELIMITER ;
+
+-- Local Variables:
+-- mode: sql
+-- eval: (sql-set-product 'mysql)
+-- End:
