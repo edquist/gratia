b45df0dc6fbd7ef1c60372789a52654441b21304
karunach <karunach@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2009-09-09 22:43:58 +0000

    * Included sites (extracted using join query from Probe, Site and VOProbeSummary tables) that report the VO not found in OIM * Formatted the report * Added subject alert tags to set proper subject lines for email
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@3384 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- reporting/summary/compareVOs.py
+++ reporting/summary/compareVOs.py
@@ -7,28 +7,68 @@
 #	 Obtain the list of VOs that is published by MyOSG
 #	 Find the difference
  
-import sys
+import sys,re
 import AccountingReports
 from AccountingReports import FromCondor,UseArgs,gOutput,gBegin,gEnd,CheckDB
 
 def compareVOs(argv=None):
     # get the list of vos reported by gratia
     gratia = AccountingReports.GetReportingVOs(AccountingReports.gBegin, AccountingReports.gEnd) 
+
     # get the list of vos reported by oim
     oim = AccountingReports.GetListOfRegisteredVO() 
     excluded = ['unknown','other']
+
     # print report header
-    print "VO discrepancy between OIM and gratia for date range: ",AccountingReports.gBegin,"to",AccountingReports.gEnd
+    print "VO discrepancy between OIM and gratia from",AccountingReports.gBegin,"to",AccountingReports.gEnd
     print
+
     # What is in gratia that is not in oim
     diff = list(set(gratia) - set(oim) - set(excluded))
-    print "1.",len(diff),"VOs reported by gratia, but not in OIM"
-    print diff
+    # Find out the sites that report these VOs. The code below creates a detailed formatted report. 
+    voStr=""
+    for vo in sorted(diff,key=str.lower):
+        voStr+="VOName=\"" + vo + "\" or "
+    voStr=re.compile("VOName=.*\"",re.IGNORECASE).search(voStr).group(0)
+    # join query to find the list of sites that reported these VOs 
+    query = "select T.SiteName, J.VOName from Site T, Probe P, VOProbeSummary J where (" + voStr + ") and P.siteid = T.siteid and J.ProbeName = P.probename and EndTime >= \"" + str(AccountingReports.gBegin) + "\" and EndTime < \"" + str(AccountingReports.gEnd) + "\" and J.ProbeName not like \"psacct:%\" group by J.VOName,T.SiteName order by lower(J.VOName),lower(T.SiteName);"
+    siteVO = AccountingReports.RunQueryAndSplit(query)
+    # If one or more VOs matched this criteria
+    if len(siteVO)!=0:
+        print "ALERT!", len(diff), "VOs reported by gratia from",AccountingReports.gBegin,"to",AccountingReports.gEnd,", but not in OIM." # alerting header that could be caught by the wrapper script to alert in the subject line of the email
+        print "Listed below are these VOs along with the sites that reported them."
+        dashLen=59; # for decoration
+        count=0
+        print dashLen*"-"
+        print(" %s  |%5s%-20s|%5s %-20s|"%("# "," ","VO"," ","SITE"))
+        print dashLen*"-"
+        for entry in siteVO:
+            count+=1
+            site = entry.split('\t')[0]
+            vo = entry.split('\t')[1]
+            if(count<10):
+                print(" %d.  |%5s%-20s|%5s %-20s|"%(count," ",vo," ",site))
+            else:
+                print(" %d. |%5s%-20s|%5s %-20s|"%(count," ",vo," ",site))
+        print dashLen*"-"
+    else:
+        print "All OK! Gratia did not report any VOs not found in OIM for",AccountingReports.gBegin,"to",AccountingReports.gEnd # alerting header that could be caught by the wrapper script to alert in the subject line of the email
+  
     print
-    # What is in oim that is not in gratia - might indicate inactivity for that particular VO in the given time range
+
+    # What is in oim that is not in gratia - might indicate inactivity for that particular VO
     diff = list(set(oim) - set(gratia) - set(excluded))
-    print "2.",len(diff),"VOs did not report"
-    print diff
+    if(len(diff) > 0):
+        print len(diff),"VOs in OIM did not report to gratia. These VOs are listed below."
+        # sort VOs in the list alphabetically ignoring case
+        for vo in sorted(diff,key=str.lower):
+            sys.stdout.write(vo)
+            sys.stdout.write("; ")
+    else:
+        print "All VOs in OIM have reported"
+ 
+    print
+    print
 
 def main(argv=None):
     # Handle command line arguments
