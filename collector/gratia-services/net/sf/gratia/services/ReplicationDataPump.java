e875a744f5740e523e1b4e6704522add22c5dc1c
greenc <greenc@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2009-10-07 17:20:53 +0000

    message is appearing too often: correct condition check.
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@3511 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- collector/gratia-services/net/sf/gratia/services/ReplicationDataPump.java
+++ collector/gratia-services/net/sf/gratia/services/ReplicationDataPump.java
@@ -295,7 +295,7 @@ public class ReplicationDataPump extends Thread {
                     replicationEntry.setrowcount(replicationEntry.getrowcount() + bundle_count);
                     session.flush();
                     session.getTransaction().commit();
-                    if (nSentThisLoop == 0) { // Message for first send
+                    if (nfirstLoopThisRun && (SentThisLoop == 0)) { // Message for first send
                         replicationLog(LogLevel.FINE, " active");
                     }
                     nSentThisLoop += bundle_count;
