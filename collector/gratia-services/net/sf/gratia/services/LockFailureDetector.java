dd848d986c9b89f97bdadc5063a6b903bae6f4ae
greenc <greenc@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2009-10-09 16:43:36 +0000

    Use static pattern compilation; fix message
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@3525 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- collector/gratia-services/net/sf/gratia/services/LockFailureDetector.java
+++ collector/gratia-services/net/sf/gratia/services/LockFailureDetector.java
@@ -1,16 +1,21 @@
 package net.sf.gratia.services;
 
 import java.text.*;
+import java.util.regex.Pattern;
+
 import org.hibernate.exception.LockAcquisitionException;
 import net.sf.gratia.util.Logging;
 
 public class LockFailureDetector {
+    
+    private static Pattern fRestartMatcher = Pattern.compile("try restarting transaction");
+
     public static Boolean detectAndReportLockFailure(Exception e, Integer nTries, String ident) {
         Throwable cause = e.getCause();
         if ((e instanceof LockAcquisitionException)
-            || ( (cause != null) && cause.getMessage().matches(".*try restarting transaction.*"))) {
+            || ( (cause != null) && fRestartMatcher.matcher(cause.getMessage()).find())) {
             if (nTries == 1) {
-                Logging.info(ident + ": lock exception.  Trying a second time.");
+                Logging.info(ident + ": lock exception: trying a second time.");
             } else if (nTries < 5) {
                 Logging.warning(ident + ": multiple contiguous lock errors: keep trying.");
             } else if (nTries == 5) {
