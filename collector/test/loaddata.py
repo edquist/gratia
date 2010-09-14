672f393a7a9ad7f1ac98cb2c6bfd43bfbbcb4c32
pcanal <pcanal@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2010-09-14 22:35:30 +0000

    add missging reference files
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@4187 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- collector/test/loaddata.py
+++ collector/test/loaddata.py
@@ -84,12 +84,12 @@ if __name__ == '__main__':
         #sendRecords(3,end)
 
         # Send several records records
-        sendRecords(500,end, "regular");
+        #sendRecords(500,end, "regular");
 
         # Send a few duplicates
-        sendRecords(20,end, "duplicate")
-        sendRecords(20,end, "duplicate")
+        sendRecords(1,end, "duplicate")
+        sendRecords(1,end, "duplicate")
 
         # Send a few record with ExtraXml
-        sendRecords(17,end,"extraxml","<RealJobName>testing extra xml</RealJobName>");
+        #sendRecords(17,end,"extraxml","<RealJobName>testing extra xml</RealJobName>");
         
