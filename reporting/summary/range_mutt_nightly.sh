a3330fbb654b738b2dda1150242ba4fa1d262bb9
pcanal <pcanal@c85a5f45-a5f3-44fc-b02c-7891c381c519>
2011-09-01 15:44:54 +0000

    remove use of INSTALL_DIR
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk@4441 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- reporting/summary/range_mutt_nightly.sh
+++ reporting/summary/range_mutt_nightly.sh
@@ -1,13 +1,13 @@
-sendtohtml $INSTALL_DIR/range "$ExtraArgs $whenarg" ${WORK_DIR}/report "$MAIL_MSG" $RECIPIENT
-sendtohtml $INSTALL_DIR/reporting "$ExtraArgs $whenarg" ${WORK_DIR}/report "$REPORTING_MAIL_MSG" $RECIPIENT
-sendtohtml $INSTALL_DIR/longjobs "$ExtraArgs $whenarg" ${WORK_DIR}/report "$LONGJOBS_MAIL_MSG" $RECIPIENT
-sendtohtml $INSTALL_DIR/usersreport "$ExtraArgs $whenarg" ${WORK_DIR}/report "$USER_MAIL_MSG" $RECIPIENT
-sendtohtml $INSTALL_DIR/efficiency "$ExtraArgs $whenarg" ${WORK_DIR}/report "${ExtraHeader}OSG Efficiency by Site and VO for $when" $RECIPIENT
-sendtohtml $INSTALL_DIR/voefficiency "$ExtraArgs $whenarg" ${WORK_DIR}/report "${ExtraHeader}OSG Efficiency by VO for $when" $RECIPIENT
-sendtohtml $INSTALL_DIR/gradedefficiency "$ExtraArgs $whenarg" ${WORK_DIR}/report "${ExtraHeader}OSG Efficiency by VO by time period for $when" $RECIPIENT
-sendtohtml $INSTALL_DIR/transfer "$ExtraArgs $whenarg" ${WORK_DIR}/report "$TR_MAIL_MSG" $RECIPIENT
-sendtohtml $INSTALL_DIR/usersitereport "$ExtraArgs $whenarg" ${WORK_DIR}/report "${ExtraHeader}Report by user by site for $when" $USER_RECIPIENT
-sendtohtml $INSTALL_DIR/compareVOs.py "$ExtraArgs $whenarg" ${WORK_DIR}/report "Subject will be set in compareVOs.py" $RECIPIENT 
+sendtohtml ./range "$ExtraArgs $whenarg" ${WORK_DIR}/report "$MAIL_MSG" $RECIPIENT
+sendtohtml ./reporting "$ExtraArgs $whenarg" ${WORK_DIR}/report "$REPORTING_MAIL_MSG" $RECIPIENT
+sendtohtml ./longjobs "$ExtraArgs $whenarg" ${WORK_DIR}/report "$LONGJOBS_MAIL_MSG" $RECIPIENT
+sendtohtml ./usersreport "$ExtraArgs $whenarg" ${WORK_DIR}/report "$USER_MAIL_MSG" $RECIPIENT
+sendtohtml ./efficiency "$ExtraArgs $whenarg" ${WORK_DIR}/report "${ExtraHeader}OSG Efficiency by Site and VO for $when" $RECIPIENT
+sendtohtml ./voefficiency "$ExtraArgs $whenarg" ${WORK_DIR}/report "${ExtraHeader}OSG Efficiency by VO for $when" $RECIPIENT
+sendtohtml ./gradedefficiency "$ExtraArgs $whenarg" ${WORK_DIR}/report "${ExtraHeader}OSG Efficiency by VO by time period for $when" $RECIPIENT
+sendtohtml ./transfer "$ExtraArgs $whenarg" ${WORK_DIR}/report "$TR_MAIL_MSG" $RECIPIENT
+sendtohtml ./usersitereport "$ExtraArgs $whenarg" ${WORK_DIR}/report "${ExtraHeader}Report by user by site for $when" $USER_RECIPIENT
+sendtohtml ./compareVOs.py "$ExtraArgs $whenarg" ${WORK_DIR}/report "Subject will be set in compareVOs.py" $RECIPIENT 
 if [ "$ExtraArgs" == "--monthly" ] ; then
-  sendtohtml $INSTALL_DIR/softwareVersions "$ExtraArgs $whenarg" ${WORK_DIR}/report "OSG Installed Probe Versions as of $when" $RECIPIENT 
+  sendtohtml ./softwareVersions "$ExtraArgs $whenarg" ${WORK_DIR}/report "OSG Installed Probe Versions as of $when" $RECIPIENT 
 fi
